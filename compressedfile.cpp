5951c615a34cf66c1a63601d53a238cd8b41e806
Georg Robbers <georg.robbers@urz.uni-hd.de>
2003-05-01 14:37:46 +0000

    don't end up with an empty tempfilename (e.g. when the realURL is only an url query) (backport rev. 1.16)
    svn path=/branches/KDE_3_1_BRANCH/kdeutils/ark/; revision=223224

--- compressedfile.cpp
+++ compressedfile.cpp
@@ -133,6 +133,8 @@ void CompressedFile::open()
   // (that code is in the slot slotOpenDone)
 
   m_tmpfile = m_gui->realURL().fileName();
+  if ( m_tmpfile.isEmpty() )
+    m_tmpfile = m_filename;
   m_tmpfile = m_tmpdir + "/" + m_tmpfile;
   KProcess proc;
   proc << "cp" << m_filename << m_tmpfile;
