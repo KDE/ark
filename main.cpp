0bc23c398dde4df4a63b145d6bf474c1e0c47dec
Laurent Montel <montel@kde.org>
2005-01-07 09:02:08 +0000

    Fix KDE_EXPORT
    svn path=/trunk/kdeutils/ark/; revision=376036

--- main.cpp
+++ main.cpp
@@ -42,7 +42,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <dcopclient.h>
 #include <kcmdlineargs.h>
 #include <kaboutdata.h>
-
+#include <kdelibs_export.h>
 // ark includes
 #include "arkapp.h"
 
