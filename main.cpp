328fa058a3384d9ac3dd0048267ee338d7f12488
Henrique Pinto <tawhaki@gmail.com>
2005-09-18 01:26:48 +0000

    Give it a version number different than the one used in 3.5
    svn path=/trunk/KDE/kdeutils/ark/; revision=461567

--- main.cpp
+++ main.cpp
@@ -65,7 +65,7 @@ static KCmdLineOptions option[] =
 extern "C" KDE_EXPORT int kdemain( int argc, char *argv[]  )
 {
 	KAboutData aboutData( "ark", I18N_NOOP( "Ark" ),
-	                      "2.6.0", I18N_NOOP( "KDE Archiving tool" ),
+	                      "2.9.9001", I18N_NOOP( "KDE Archiving tool" ),
 	                      KAboutData::License_GPL,
 	                      I18N_NOOP( "(c) 1997-2005, The Various Ark Developers" )
 	                    );
