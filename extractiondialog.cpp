1c38f7c70bc3501a60b4c4090ceb0a796e0c78bb
Charis Kouzinopoulos <kouzinopoulos@gmail.com>
2006-04-07 00:11:34 +0000

    Usability improvement. Don't repeat the text in the buttons
    svn path=/branches/KDE/3.5/kdeutils/ark/; revision=527139

--- extractiondialog.cpp
+++ extractiondialog.cpp
@@ -146,7 +146,7 @@ void ExtractionDialog::accept()
 		if ( !fi.isDir() && !fi.exists() )
 		{
 			QString ltext = i18n( "Create folder %1?").arg(p.path());
-			int createDir =  KMessageBox::questionYesNo( this, ltext, i18n( "Missing Folder" ) , i18n("Create Folder"), i18n("Do Not Create"));
+			int createDir =  KMessageBox::questionYesNo( this, ltext, i18n( "Missing Folder" ));
 			if( createDir == 4 )
 			{
 				return;
