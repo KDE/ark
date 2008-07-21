3c5f7ffdadd09a818d2c966b53f1ed867f4f4e9a
Friedrich W. H. Kossebau <kossebau@kde.org>
2008-07-21 16:13:39 +0000

    Updating the version number and adding link to homepage on utils.kde.org
    CCMAIL: kde-utils-devel@kde.org
    
    svn path=/trunk/KDE/kdeutils/ark/; revision=836046

--- app/main.cpp
+++ app/main.cpp
@@ -30,9 +30,11 @@
 int main( int argc, char **argv )
 {
 	KAboutData aboutData( "ark", 0, ki18n( "Ark" ),
-	                      "2.9.999", ki18n( "KDE Archiving tool" ),
+	                      "2.11.999", ki18n( "KDE Archiving tool" ),
 	                      KAboutData::License_GPL,
-	                      ki18n( "(c) 1997-2007, The Various Ark Developers" )
+	                      ki18n( "(c) 1997-2008, The Various Ark Developers" ),
+	                      KLocalizedString(),
+	                      "http://utils.kde.org/projects/ark"
 	                    );
 
 	aboutData.addAuthor( ki18n("Henrique Pinto"),
