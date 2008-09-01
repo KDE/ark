7132ab798e0b5b718c4a7003412f9e702a47c84d
Pino Toscano <pino@kde.org>
2008-09-01 17:14:00 +0000

    extract the messages from the .rc files too
    svn path=/trunk/KDE/kdeutils/ark/; revision=855890

--- Messages.sh
+++ Messages.sh
@@ -1,2 +1,4 @@
 #! /bin/sh
-$XGETTEXT app/*.cpp kerfuffle/*.cpp part/*.cpp plugins/*/*.cpp -o $podir/ark.pot
+$EXTRACTRC $(find . -name '*.rc') >> rc.cpp || exit 11
+$XGETTEXT app/*.cpp kerfuffle/*.cpp part/*.cpp plugins/*/*.cpp rc.cpp -o $podir/ark.pot
+rm -f rc.cpp
