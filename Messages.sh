a5cf5a3755956b58a5a65c1cf0fc90018f4ac01f
Pino Toscano <pino@kde.org>
2008-09-02 10:24:08 +0000

    extract messages from .ui files too
    svn path=/trunk/KDE/kdeutils/ark/; revision=856184

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 $EXTRACTRC $(find . -name '*.rc') >> rc.cpp || exit 11
+$EXTRACTRC $(find . -name '*.ui') >> rc.cpp || exit 12
 $XGETTEXT app/*.cpp kerfuffle/*.cpp part/*.cpp plugins/*/*.cpp rc.cpp -o $podir/ark.pot
 rm -f rc.cpp
