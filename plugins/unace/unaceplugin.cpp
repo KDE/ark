d34dc60c5706a7d51238bef256333906dafdeecd
Rafał Rzepecki <divided.mind@gmail.com>
2009-02-04 10:42:34 +0000

    Stub of UNACE interface.
    svn path=/trunk/KDE/kdeutils/ark/; revision=921022

---
+++ plugins/unace/unaceplugin.cpp
@@ -0,0 +1,46 @@
+/*
+ * unace plugin for ark -- archiver for the KDE project
+ *
+ * Copyright (C) 2009 Rafał Rzepecki <divided.mind@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#include <kerfuffle/archivefactory.h>
+
+#include "unaceplugin.h"
+
+UnAceInterface::UnAceInterface( const QString & filename, QObject *parent )
+		: ReadOnlyArchiveInterface( filename, parent )
+{}
+
+UnAceInterface::~UnAceInterface()
+{}
+
+bool UnAceInterface::list()
+{
+	return false;
+}
+
+bool UnAceInterface::copyFiles( const QList<QVariant> & files, const QString & destinationDirectory, ExtractionOptions options )
+{
+	return false;
+}
+
+#include "unaceplugin.moc"
+
+KERFUFFLE_PLUGIN_FACTORY( UnAceInterface )
+
