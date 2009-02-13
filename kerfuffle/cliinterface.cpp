81ba4cc1fdf021fda3f07bd675ef7bac444662ec
Harald Hvaal <haraldhv@stud.ntnu.no>
2009-02-13 15:55:46 +0000

    Adding the cliInterface class, along with a plugin using it. so far it is hardcoded to use rar, but it will be expanded to cover a wide range of filetypes once finished.
    svn path=/trunk/KDE/kdeutils/ark/; revision=925589

---
+++ kerfuffle/cliinterface.cpp
@@ -0,0 +1,239 @@
+/*
+ * ark -- archiver for the KDE project
+ *
+ * Copyright (C) 2009 Harald Hvaal
+ * <haraldhv atatatat stud.ntnu.no>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+#include "cliinterface.h"
+
+#include <QFile>
+#include <QDir>
+#include <QDateTime>
+#include <KProcess>
+#include <KStandardDirs>
+#include <KDebug>
+#include <KLocale>
+#include <QEventLoop>
+#include <QThread>
+#include <KProcess>
+#include <kptyprocess.h>
+#include <kptydevice.h>
+
+namespace Kerfuffle
+{
+
+	CliInterface::CliInterface( const QString& filename, QObject *parent)
+		: ReadWriteArchiveInterface(filename, parent),
+		m_process(NULL),
+		m_loop(NULL)
+	{
+
+
+	}
+
+	CliInterface::~CliInterface()
+	{
+
+	}
+
+	bool CliInterface::list()
+	{
+		ParameterList param = parameterList();
+
+		Q_ASSERT(param.contains(ListProgram));
+
+		bool ret = findProgramInPath(param.value(ListProgram).toString());
+		if (!ret) {
+			error("TODO could not find program");
+			return false;
+		}
+
+		ret = createProcess();
+		if (!ret) {
+			error("TODO could not find program");
+			return false;
+		}
+
+		QStringList args = param.value(ListArgs).toStringList();
+		substituteVariables(args);
+
+		executeProcess(m_program, args);
+
+		return true;
+	}
+
+	bool CliInterface::copyFiles( const QList<QVariant> & files, const QString & destinationDirectory, ExtractionOptions options )
+	{
+		return false;
+	}
+
+
+	bool CliInterface::addFiles( const QStringList & files, const CompressionOptions& options )
+	{
+
+		return false;
+	}
+
+	bool CliInterface::deleteFiles( const QList<QVariant> & files )
+	{
+
+		return false;
+	}
+
+	bool CliInterface::createProcess()
+	{
+		if (m_process)
+			return false;
+
+		m_process = new KPtyProcess;
+		m_process->setOutputChannelMode( KProcess::SeparateChannels );
+
+		connect( m_process, SIGNAL( started() ), SLOT( started() ) );
+		connect( m_process, SIGNAL( readyReadStandardOutput() ), SLOT( readStdout() ) );
+		connect( m_process, SIGNAL( readyReadStandardError() ), SLOT( readFromStderr() ) );
+		connect( m_process, SIGNAL( finished( int, QProcess::ExitStatus ) ), SLOT( finished( int, QProcess::ExitStatus ) ) );
+
+		if (QMetaType::type("QProcess::ExitStatus") == 0)
+			qRegisterMetaType<QProcess::ExitStatus>("QProcess::ExitStatus");
+		return true;
+	}
+
+	bool CliInterface::executeProcess(const QString& path, const QStringList & args)
+	{
+		Q_ASSERT(!path.isEmpty());
+
+		kDebug( 1601 ) << "Executing " << path << args;
+
+		m_process->setProgram( path, args );
+		m_process->setNextOpenMode( QIODevice::ReadWrite | QIODevice::Unbuffered );
+		m_process->start();
+		QEventLoop loop;
+		m_loop = &loop;
+		bool ret = loop.exec( QEventLoop::WaitForMoreEvents );
+		m_loop = 0;
+
+		delete m_process;
+		m_process = NULL;
+		/*
+		   if (!m_errorMessages.isEmpty())
+		   {
+		   error(m_errorMessages.join("\n"));
+		   return false;
+		   }
+		   else if (ret && !m_userCancelled) {
+		   error(i18n("Unknown error when extracting files"));
+		   return false;
+		   }
+		   else
+		   {
+		   return true;
+		   }
+		   */
+
+		return true;
+	}
+
+
+	void CliInterface::started()
+	{
+		//m_state = 0;
+		m_errorMessages.clear();
+		m_userCancelled = false;
+	}
+
+	void CliInterface::finished( int exitCode, QProcess::ExitStatus exitStatus)
+	{
+		if ( !m_process )
+			return;
+
+		progress(1.0);
+
+		if ( m_loop )
+		{
+			m_loop->exit( exitStatus == QProcess::CrashExit ? 1 : 0 );
+		}
+	}
+
+	void CliInterface::readFromStderr()
+	{
+		if ( !m_process )
+			return;
+
+		QByteArray stdErrData = m_process->readAllStandardError();
+
+		kDebug( 1601 ) << "ERROR" << stdErrData.size() << stdErrData;
+
+		if ( !stdErrData.isEmpty() )
+		{
+			//if (handlePasswordPrompt(stdErrData))
+				//return;
+			//else if (handleOverwritePrompt(stdErrData))
+			//	return;
+			//else
+			{
+				m_errorMessages << QString::fromLocal8Bit(stdErrData);
+			}
+		}
+	}
+
+	void CliInterface::readStdout()
+	{
+		if ( !m_process )
+			return;
+
+		m_stdOutData += m_process->readAllStandardOutput();
+
+		// process all lines until the last '\n' or backspace
+		int indx = m_stdOutData.lastIndexOf('\010');
+		if (indx == -1) indx = m_stdOutData.lastIndexOf('\n');
+		if (indx == -1) return;
+
+		QString leftString = QString::fromLocal8Bit(m_stdOutData.left(indx + 1));
+		const QStringList lines = leftString.split( QRegExp("[\\n\\010]"), QString::SkipEmptyParts );
+		foreach(const QString &line, lines) {
+
+			kDebug( 1601 ) << line;
+
+		}
+
+		m_stdOutData.remove(0, indx + 1);
+	}
+
+
+	bool CliInterface::findProgramInPath(const QString& program)
+	{
+		m_program = KStandardDirs::findExe( program );
+		return !m_program.isEmpty();
+	}
+
+	void CliInterface::substituteVariables(QStringList& params)
+	{
+		for (int i = 0; i < params.size(); ++i) {
+			QString parameter = params.at(i);
+
+			if (parameter == "$Archive") {
+				params[i] = filename();
+			}
+
+		}
+	}
+
+}
+
+#include "cliinterface.moc"
---
+++ kerfuffle/cliinterface.h
@@ -0,0 +1,155 @@
+/*
+ * ark -- archiver for the KDE project
+ *
+ * Copyright (C) 2009 Harald Hvaal
+ * <haraldhv atatatat stud.ntnu.no>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+#ifndef _CLIINTERFACE_H_
+#define _CLIINTERFACE_H_
+
+#include "archiveinterface.h"
+#include "kerfuffle_export.h"
+#include <kptyprocess.h>
+#include <QEventLoop>
+
+namespace Kerfuffle
+{
+
+	enum CliInterfaceExtractOptions {
+		/**
+		 * When passing directories to the extract program, do not
+		 * include trailing slashes
+		 * e.g. if the user selected "foo/" and "foo/bar" in the gui, the
+		 * paths "foo" and "foo/bar" will be sent to the program.
+		 * The default is to include trailing slashes
+		 */
+		NoTrailingSlashes = 0
+	};
+
+	enum CliInterfaceParameters {
+		/**
+		 * QString
+		 * The name to the program that will handle listing of this
+		 * archive (eg "rar"). Will be searched for in PATH
+		 */
+		ListProgram = 0,
+		/**
+		 * QStringList
+		 * The arguments that are passed to the program above for
+		 * listing the archive. Special strings that will be
+		 * substituted:
+		 * $Archive - the path of the archive
+		 */
+		ListArgs,
+		/**
+		 * QString
+		 * The name to the program that will handle extracting of this
+		 * archive (eg "rar"). Will be searched for in PATH
+		 */
+		ExtractProgram,
+		/**
+		 * QStringList
+		 * The arguments that are passed to the program above for
+		 * extracting the archive. Special strings that will be
+		 * substituted:
+		 * $Archive - the path of the archive
+		 * $Files - the files selected to be extracted, if any
+		 */
+		ExtractArgs,
+		/**
+		 * QVariantList
+		 * A list of additional options for the extract operation, see
+		 * the CliInterfaceExtractOptions enum above.
+		 */
+		ExtractOptions,
+		/**
+		 * QString
+		 * The name to the program that will handle deleting of elements in this
+		 * archive format (eg "rar"). Will be searched for in PATH
+		 */
+		DeleteProgram,
+		/**
+		 * QStringList
+		 * The arguments that are passed to the program above for
+		 * deleting from the archive. Special strings that will be
+		 * substituted:
+		 * $Archive - the path of the archive
+		 * $Files - the files selected to be deleted
+		 */
+		DeleteArgs,
+		/**
+		 * QString
+		 * The name to the program that will handle adding in this
+		 * archive format (eg "rar"). Will be searched for in PATH
+		 */
+		AddProgram,
+		/**
+		 * QStringList
+		 * The arguments that are passed to the program above for
+		 * adding to the archive. Special strings that will be
+		 * substituted:
+		 * $Archive - the path of the archive
+		 * $Files - the files selected to be added
+		 */
+		AddArgs
+	};
+
+	typedef QHash<int, QVariant> ParameterList;
+
+	class KERFUFFLE_EXPORT CliInterface : public ReadWriteArchiveInterface
+	{
+		Q_OBJECT
+
+		public:
+			explicit CliInterface( const QString& filename, QObject *parent = 0);
+			virtual ~CliInterface();
+
+			virtual bool list();
+			virtual bool copyFiles( const QList<QVariant> & files, const QString & destinationDirectory, ExtractionOptions options );
+			virtual bool addFiles( const QStringList & files, const CompressionOptions& options );
+			virtual bool deleteFiles( const QList<QVariant> & files );
+
+			virtual ParameterList parameterList() const = 0;
+
+		private:
+			bool findProgramInPath(const QString& program);
+			void substituteVariables(QStringList& params);
+
+			bool createProcess();
+			bool executeProcess(const QString& path, const QStringList & args);
+
+			QStringList m_errorMessages;
+			QByteArray m_stdOutData;
+			QList<QVariant> m_archiveContents;
+			bool m_userCancelled;
+
+			KPtyProcess *m_process;
+			QString m_program;
+			QEventLoop *m_loop;
+
+		private slots:
+			void started();
+			void readStdout();
+			void readFromStderr();
+			void finished( int exitCode, QProcess::ExitStatus exitStatus );
+
+	};
+
+}
+
+#endif /* _CLIINTERFACE_H_ */
