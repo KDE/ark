429e718c157abe9bc3cdd0ac55ab0bbb63542a3e
Raphael Kubo da Costa <kubito@gmail.com>
2009-04-21 05:51:53 +0000

    Split clirarplugin into a header and a source file, add missing includes.
    svn path=/trunk/KDE/kdeutils/ark/; revision=956954

--- plugins/clirarplugin/cliplugin.cpp
+++ plugins/clirarplugin/cliplugin.cpp
@@ -18,140 +18,136 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  */
-#include "kerfuffle/cliinterface.h"
-#include "kerfuffle/archivefactory.h"
-#include <kdebug.h>
 
+#include "cliplugin.h"
+#include "kerfuffle/archivefactory.h"
+#include "kerfuffle/cliinterface.h"
+
+#include <QDateTime>
 #include <QDir>
+#include <QString>
+#include <QStringList>
+
+#include <KDebug>
 
 using namespace Kerfuffle;
 
-class CliPlugin: public CliInterface
+CliPlugin::CliPlugin( const QString & filename, QObject *parent )
+	: CliInterface( filename, parent )
+	, m_isFirstLine(true)
+	, m_isInContentListing(false)
 {
-	public:
-		explicit CliPlugin( const QString & filename, QObject *parent = 0 )
-			: CliInterface( filename, parent ),
-			m_isFirstLine(true),
-			m_incontent(false)
-		{
+}
 
-		}
+CliPlugin::~CliPlugin()
+{
+}
 
-		virtual ~CliPlugin()
-		{
+ParameterList CliPlugin::parameterList() const
+{
+	static ParameterList p;
 
-		}
+	if (p.isEmpty()) {
+		p[CaptureProgress] = true;
+		p[ListProgram] = p[ExtractProgram] = p[DeleteProgram] = p[AddProgram] = "rar";
 
-		virtual ParameterList parameterList() const
-		{
-			static ParameterList p;
-			if (p.isEmpty()) {
+		p[ListArgs] = QStringList() << "v" << "-c-" << "$Archive";
+		p[ExtractArgs] = QStringList() << "-p-" << "$PreservePathSwitch" << "$PasswordSwitch" << "$RootNodeSwitch" << "$Archive" << "$Files";
+		p[PreservePathSwitch] = QStringList() << "x" << "e";
+		p[RootNodeSwitch] = QStringList() << "-ap$Path";
+		p[PasswordSwitch] = QStringList() << "-p$Password";
 
-				p[CaptureProgress] = true;
-				p[ListProgram] = p[ExtractProgram] = p[DeleteProgram] = p[AddProgram] = "rar";
+		p[DeleteArgs] = QStringList() << "d" << "$Archive" << "$Files";
 
-				p[ListArgs] = QStringList() << "v" << "-c-" << "$Archive";
-				p[ExtractArgs] = QStringList() << "-p-" << "$PreservePathSwitch" << "$PasswordSwitch" << "$RootNodeSwitch" << "$Archive" << "$Files";
-				p[PreservePathSwitch] = QStringList() << "x" << "e";
-				p[RootNodeSwitch] = QStringList() << "-ap$Path";
-				p[PasswordSwitch] = QStringList() << "-p$Password";
+		p[FileExistsExpression] = "^(.+) already exists. Overwrite it";
+		p[FileExistsInput] = QStringList()
+			<< "Y" //overwrite
+			<< "N" //skip
+			<< "A" //overwrite all
+			<< "E" //autoskip
+			<< "Q" //cancel
+			;
 
-				p[DeleteArgs] = QStringList() << "d" << "$Archive" << "$Files";
+		p[AddArgs] = QStringList() << "a" << "$Archive" << "$Files";
 
-				p[FileExistsExpression] = "^(.+) already exists. Overwrite it";
-				p[FileExistsInput] = QStringList()
-					<< "Y" //overwrite
-					<< "N" //skip
-					<< "A" //overwrite all
-					<< "E" //autoskip
-					<< "Q" //cancel
-					;
+		p[WrongPasswordPatterns] = QStringList() << "password incorrect";
+		p[ExtractionFailedPatterns] = QStringList() << "CRC failed";
+	}
 
-				p[AddArgs] = QStringList() << "a" << "$Archive" << "$Files";
+	return p;
+}
 
-				p[WrongPasswordPatterns] = QStringList() << "password incorrect";
-				p[ExtractionFailedPatterns] = QStringList() << "CRC failed";
+bool CliPlugin::readListLine(QString line)
+{
+	const QString m_headerString = "-----------------------------------------";
 
-			}
-			return p;
-		}
+	// skip the heading
+	if (!m_isInContentListing){
+		if (line.startsWith(m_headerString) )
+			m_isInContentListing = true;
+		return true;
+	}
 
+	// catch final line
+	if (line.startsWith(m_headerString) ) {
+		m_isInContentListing = false;
+		return true;
+	}
 
-
-
-		bool m_isFirstLine, m_incontent, m_isPasswordProtected;
-		QString m_entryFilename, m_internalId;
-
-		bool readListLine(QString line)
-		{
-
-			const QString m_headerString = "-----------------------------------------";
-			// skip the heading
-			if (!m_incontent){
-				if (line.startsWith(m_headerString) )
-					m_incontent = true;
-				return true;
-			}
-			// catch final line
-			if (line.startsWith(m_headerString) ) {
-				m_incontent = false;
-				return true;
-			}
-
-			// rar gives one line for the filename and a line after it with some file properties
-			if ( m_isFirstLine ) {
-				m_internalId = line.trimmed();
-				//m_entryFilename.chop(1); // handle newline
-				if (!m_internalId.isEmpty() && m_internalId.at(0) == '*')
-				{
-					m_isPasswordProtected = true;
-					m_internalId.remove( 0, 1 ); // and the spaces in front
-				}
-				else
-					m_isPasswordProtected = false;
-
-				m_isFirstLine = false;
-				return true;
-			}
-
-			QStringList fileprops = line.split(' ', QString::SkipEmptyParts);
-			m_internalId = QDir::fromNativeSeparators(m_internalId);
-			bool isDirectory = (bool)(fileprops[ 5 ].contains('d', Qt::CaseInsensitive));
-
-			QDateTime ts (QDate::fromString(fileprops[ 3 ], "dd-MM-yy"),
-					QTime::fromString(fileprops[ 4 ], "hh:mm"));
-			// rar output date with 2 digit year but QDate takes is as 19??
-			// let's take 1950 is cut-off; similar to KDateTime
-			if (ts.date().year() < 1950)
-				ts = ts.addYears(100);
-
-			m_entryFilename = m_internalId;
-			if (isDirectory && !m_internalId.endsWith('/'))
+	// rar gives one line for the filename and a line after it with some file properties
+	if ( m_isFirstLine ) {
+		m_internalId = line.trimmed();
+		//m_entryFilename.chop(1); // handle newline
+		if (!m_internalId.isEmpty() && m_internalId.at(0) == '*')
 			{
-				m_entryFilename += '/';
+			m_isPasswordProtected = true;
+			m_internalId.remove( 0, 1 ); // and the spaces in front
 			}
+		else
+			m_isPasswordProtected = false;
 
-			//kDebug( 1601 ) << m_entryFilename << " : " << fileprops ;
-			ArchiveEntry e;
-			e[ FileName ] = m_entryFilename;
-			e[ InternalID ] = m_internalId;
-			e[ Size ] = fileprops[ 0 ];
-			e[ CompressedSize] = fileprops[ 1 ];
-			e[ Ratio ] = fileprops[ 2 ];
-			e[ Timestamp ] = ts;
-			e[ IsDirectory ] = isDirectory;
-			e[ Permissions ] = fileprops[ 5 ].remove(0,1);
-			e[ CRC ] = fileprops[ 6 ];
-			e[ Method ] = fileprops[ 7 ];
-			e[ Version ] = fileprops[ 8 ];
-			e[ IsPasswordProtected] = m_isPasswordProtected;
-			kDebug( 1601 ) << "Added entry: " << e ;
+		m_isFirstLine = false;
+		return true;
+	}
 
-			entry(e);
-			m_isFirstLine = true;
-			return true;
-		}
-};
+	QStringList fileprops = line.split(' ', QString::SkipEmptyParts);
+	m_internalId = QDir::fromNativeSeparators(m_internalId);
+	bool isDirectory = (bool)(fileprops[ 5 ].contains('d', Qt::CaseInsensitive));
+
+	QDateTime ts (QDate::fromString(fileprops[ 3 ], "dd-MM-yy"),
+		QTime::fromString(fileprops[ 4 ], "hh:mm"));
+	// rar output date with 2 digit year but QDate takes is as 19??
+	// let's take 1950 is cut-off; similar to KDateTime
+	if (ts.date().year() < 1950)
+		ts = ts.addYears(100);
+
+	m_entryFilename = m_internalId;
+	if (isDirectory && !m_internalId.endsWith('/'))
+	{
+		m_entryFilename += '/';
+	}
+
+	//kDebug( 1601 ) << m_entryFilename << " : " << fileprops ;
+	ArchiveEntry e;
+	e[ FileName ] = m_entryFilename;
+	e[ InternalID ] = m_internalId;
+	e[ Size ] = fileprops[ 0 ];
+	e[ CompressedSize] = fileprops[ 1 ];
+	e[ Ratio ] = fileprops[ 2 ];
+	e[ Timestamp ] = ts;
+	e[ IsDirectory ] = isDirectory;
+	e[ Permissions ] = fileprops[ 5 ].remove(0,1);
+	e[ CRC ] = fileprops[ 6 ];
+	e[ Method ] = fileprops[ 7 ];
+	e[ Version ] = fileprops[ 8 ];
+	e[ IsPasswordProtected] = m_isPasswordProtected;
+	kDebug( 1601 ) << "Added entry: " << e ;
+
+	entry(e);
+	m_isFirstLine = true;
+	return true;
+}
 
 KERFUFFLE_PLUGIN_FACTORY(CliPlugin)
 
+#include "cliplugin.moc"
---
+++ plugins/clirarplugin/cliplugin.h
@@ -0,0 +1,53 @@
+/*
+ * ark -- archiver for the KDE project
+ *
+ * Copyright (C) 2009 Harald Hvaal <haraldhv@stud.ntnu.no>
+ * Copyright (C) 2009 Raphael Kubo da Costa <kubito@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+#ifndef CLIPLUGIN_H
+#define CLIPLUGIN_H
+
+#include "kerfuffle/cliinterface.h"
+
+class CliPlugin : public Kerfuffle::CliInterface
+{
+	Q_OBJECT
+
+	public:
+		explicit CliPlugin( const QString & filename, QObject *parent = 0 );
+
+		virtual ~CliPlugin();
+
+		virtual Kerfuffle::ParameterList parameterList() const;
+
+		virtual bool readListLine(QString line);
+
+	protected:
+		QString m_entryFilename;
+
+		QString m_internalId;
+
+		bool m_isFirstLine;
+
+		bool m_isInContentListing;
+
+		bool m_isPasswordProtected;
+};
+
+#endif // CLIPLUGIN_H
