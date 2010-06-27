0fe5af94ff5604062161d749db122ba5aef55a6b
Raphael Kubo da Costa <kubito@gmail.com>
2010-06-27 01:31:19 +0000

    Update copyright years.
    svn path=/trunk/KDE/kdeutils/ark/; revision=1143227

--- kerfuffle/jobs.h
+++ kerfuffle/jobs.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2007 Henrique Pinto <henrique.pinto@kdemail.net>
  * Copyright (c) 2008-2009 Harald Hvaal <haraldhv@stud.ntnu.no>
- * Copyright (c) 2009 Raphael Kubo da Costa <kubito@gmail.com>
+ * Copyright (c) 2009-2010 Raphael Kubo da Costa <kubito@gmail.com>
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
