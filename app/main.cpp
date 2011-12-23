d2acda93d47f89e9ed3ee925039b566ae8c4026e
Raphael Kubo da Costa <rakuco@FreeBSD.org>
2011-12-22 23:44:16 -0200

    Bump version now that 4.8 has been branched.

--- app/main.cpp
+++ app/main.cpp
@@ -38,7 +38,7 @@ using Kerfuffle::AddToArchive;
 int main(int argc, char **argv)
 {
     KAboutData aboutData("ark", 0, ki18n("Ark"),
-                         "2.18", ki18n("KDE Archiving tool"),
+                         "2.19", ki18n("KDE Archiving tool"),
                          KAboutData::License_GPL,
                          ki18n("(c) 1997-2011, The Various Ark Developers"),
                          KLocalizedString(),
