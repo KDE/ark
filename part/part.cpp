5da88ce09e0a398f1d233633733ea080546fa5ed
Lamarque V. Souza <Lamarque.Souza.ext@basyskom.com>
2012-07-27 01:20:54 +0200

    Fix copy&paste from Ark to other KDE programs (Dolphin for example). Copy&paste from other KDE programs to Ark already works. This fix uses the p7zip kio slave we implemented in kde-runtime.

--- part/part.cpp
+++ part/part.cpp
@@ -1558,8 +1558,34 @@ void Part::cut()
 
 void Part::copy()
 {
+    if (!m_model || !m_model->archive()) {
+        return;
+    }
+
+    QString fileName = m_model->archive()->fileName();
+    QString protocol = fileName.mid(protocol.lastIndexOf(QLatin1Char('.')));
+
+    if (protocol == QLatin1String("rar")) {
+        protocol = "ar";
+    } else if (protocol == QLatin1String("zip")) {
+        protocol = "zip";
+    } else if (protocol == QLatin1String("tar")) {
+        protocol = "tar";
+    } else {
+        protocol = "p7zip";
+    }
+    
+    QList<QUrl> selectedFiles;
+    foreach (const QModelIndex & index, m_archiveView->selectionModel()->selectedRows()) {
+        const ArchiveEntry& entry = m_model->entryForIndex(index);
+        QUrl u(protocol + QLatin1String("://") + fileName + QLatin1Char('/') + entry[FileName].toString());
+        kDebug(1601) << "going to add entry" << u;
+        selectedFiles.append(u);
+    }
+
     QMimeData* mimeData = new QMimeData();
     populateMimeData(mimeData, false);
+    mimeData->setUrls(selectedFiles);
     QApplication::clipboard()->setMimeData(mimeData);
 }
 
