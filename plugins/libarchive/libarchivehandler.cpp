KDE/ark

commit 413d9bbceafe83e6afea99ebb79ac356d0ec88b3

libarchive: Artificially add a trailing slash for rootNodes.
Some tarballs (such as the one I have for asciidoc 8.6.4) only report the
files in it, so the directories need to be created manually by Ark. However,
this means the ArchiveEntry created in ArchiveModel::parentFor() will not
have a trailing slash, and so will the path passed as RootNode to
LibArchiveInterface::copyFiles(). The truncation that happens later will
then leave a slash at the beginning if we're not extracting a top-level
entry, which will simply make drag'n'drop extraction fail.

plugins/libarchive/libarchivehandler.cpp
@@ -138,8 +138,10 @@ bool LibArchiveInterface::copyFiles(const QVariantList& files, const QString& de
     const bool extractAll = files.isEmpty();
     const bool preservePaths = options.value(QLatin1String( "PreservePaths" )).toBool();
 
-    const QString rootNode = options.value(QLatin1String( "RootNode" ), QVariant()).toString();
-    kDebug() << "Set root node" << rootNode;
+    QString rootNode = options.value(QLatin1String("RootNode"), QVariant()).toString();
+    if (!rootNode.endsWith(QLatin1Char('/'))) {
+        rootNode.append(QLatin1Char('/'));
+    }
 
     ArchiveRead arch(archive_read_new());
 
@@ -240,9 +242,9 @@ bool LibArchiveInterface::copyFiles(const QVariantList& files, const QString& de
                 //OR, if the commonBase has been set, then we remove this
                 //common base from the filename
             } else if (!rootNode.isEmpty()) {
-                const QString truncatedFilename(entryName.remove(0, rootNode.size()));
-                kDebug() << "Truncated filename: " << truncatedFilename;
+                kDebug() << "Removing" << rootNode << "from" << entryName;
 
+                const QString truncatedFilename(entryName.remove(0, rootNode.size()));
                 archive_entry_copy_pathname(entry, QFile::encodeName(truncatedFilename).constData());
 
                 entryFI = QFileInfo(truncatedFilename);

commit fa91943887449986c645e1c6d215bc503483baf6

libarchive: Only add a trailing slash for rootNodes if there is a rootNode.
Fix regression introduced in 413d9bb: if rootNode is an empty string,
it should remain empty, otherwise we artificially create a rootNode
and truncation fails.

plugins/libarchive/libarchivehandler.cpp
@@ -139,7 +139,7 @@ bool LibArchiveInterface::copyFiles(const QVariantList& files, const QString& de
     const bool preservePaths = options.value(QLatin1String( "PreservePaths" )).toBool();
 
     QString rootNode = options.value(QLatin1String("RootNode"), QVariant()).toString();
-    if (!rootNode.endsWith(QLatin1Char('/'))) {
+    if ((!rootNode.isEmpty()) && (!rootNode.endsWith(QLatin1Char('/')))) {
         rootNode.append(QLatin1Char('/'));
     }
 