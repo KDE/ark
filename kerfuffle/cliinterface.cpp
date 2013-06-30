KDE/ark

commit c7af2d6372ea2b222e1af223b3fd53cdeb1f3c2b

Stop emitting finished() from CliInterface::failOperation().
We were always emitting finished() twice if failOperation() was
called:
 - If it did kill the process in its doKill() call, the fact that the
   process was killed would call processFinished(), which would emit
   finished(true). failOperation() would later emit finished(false).
 - For the cases in which the process was not being killed, the
   callers in cliinterface.cpp were acting according to the following
   pattern:

       if (something failed) {
           emit error("Oops, something bad happened");
           failOperation();
           return false;
       }

   If the process wasn't killed it means it had already finished, so
   processFinished() had already been called (and consequently
   finished(true) had already been emitted), yet failOperation()
   would emit finished(false) again.

This was particularly troublesome for AddToArchive and BatchExtract,
since they rely on the KJob::result() signal to kill the
AddToArchive/BatchExtract instance and quit the application. Since
our Jobs connect to the finished() signal to call KJob::emitResult(),
the application was being deleted by the time the second finished()
signal was emitted, leading to crashes (this normally happens if you
enter a wrong password or extract a corrupted file).

Unfortunately, we still don't unit test CliInterface, so no there are
no new tests :(

BUG: 304764
BUG: 304178
FIXED-IN: 4.9.1

kerfuffle/cliinterface.cpp
@@ -403,11 +403,9 @@ void CliInterface::processFinished(int exitCode, QProcess::ExitStatus exitStatus
 
 void CliInterface::failOperation()
 {
+    // TODO: Would be good to unit test #304764/#304178.
     kDebug();
-
     doKill();
-
-    emit finished(false);
 }
 
 void CliInterface::readStdout(bool handleAll)

commit a3736e60172d604144b5eee7c62600066f6e2d6c

Emit finished() when runProcess does not find any program.
This fixes a regression introduced in c7af2d6: while the `finished' signal
will be emitted correctly in most cases, if none of the executables passed
to CliInterface::runProcess() are found only the `error' signal would be
sent, and the program would wait for a `finished' signal forever and never
notify the user that the required programs were not found.

Submitted by:	Ádám István Mózes <sajtos@sch.bme.hu>
BUG:		319663
REVIEW:		111318
FIXED-IN:	4.11.0

kerfuffle/cliinterface.cpp
@@ -330,6 +330,7 @@ bool CliInterface::runProcess(const QStringList& programNames, const QStringList
         const QString names = programNames.join(QLatin1String(", "));
         emit error(i18ncp("@info", "Failed to locate program <filename>%2</filename> on disk.",
                                    "Failed to locate programs <filename>%2</filename> on disk.", programNames.count(), names));
+        emit finished(false);
         return false;
     }
 