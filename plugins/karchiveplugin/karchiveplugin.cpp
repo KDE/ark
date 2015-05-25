02523ed829bcff6b93670a912f39702f904801fe
Ragnar Thomsen <rthomsen6@gmail.com>
2015-05-25 16:14:33 +0200

    Set permissions for files when extracting with karchive plugin.
    Uses KIO::convertPermissions to convert permissions from st_mode type used by karchive to QFileDevice::Permissions used by QFile.
    
    SUID, GUID and sticky bit are not set.

--- plugins/karchiveplugin/karchiveplugin.cpp
+++ plugins/karchiveplugin/karchiveplugin.cpp
@@ -27,6 +27,7 @@
 #include <KTar>
 #include <KLocalizedString>
 #include <KPluginFactory>
+#include <KIO/Global>
 
 #include <QDebug>
 #include <QFileInfo>
@@ -206,6 +207,7 @@ bool KArchiveInterface::copyFiles(const QList<QVariant> &files, const QString &d
 
                 if (archiveEntry->symLinkTarget().isEmpty()) {
                     static_cast<const KArchiveFile*>(archiveEntry)->copyTo(realDestination);
+                    QFile::setPermissions(realDestination + QStringLiteral("/") + archiveEntry->name(), KIO::convertPermissions(archiveEntry->permissions()));
                 }
                 else {
                     // Create symlink
