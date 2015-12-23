KDE/ark

commit 3a22bbd5ecfe409023088aa3e71e3151cc442860

Add support for reading archive comments
Add support for reading comments in CLI-based plugins such as rar and
zip. The comment is parsed from the output of ListJob. The comment is
displayed below the file view in a read-only QPlainTextEdit with
monospace font. A QSplitter is used so the user can resize the comment
field.

Support is added to clirar, clizip and cli7z plugins. The 7z-format
doesn't support comments, but the cli7z-plugin supports reading comments
in e.g. zip archives.

BUG: 328790
FIXED-IN: 15.12.0
REVIEW: 124468

part/part.cpp
@@ -67,6 +67,8 @@
 #include <QIcon>
 #include <QInputDialog>
 #include <QFileSystemWatcher>
+#include <QGroupBox>
+#include <QPlainTextEdit>
 
 using namespace Kerfuffle;
 
@@ -101,6 +103,16 @@ Part::Part(QWidget *parentWidget, QObject *parent, const QVariantList& args)
     m_view = new ArchiveView;
     m_infoPanel = new InfoPanel(m_model);
 
+    // Add widgets for the comment field.
+    m_commentView = new QPlainTextEdit();
+    m_commentView->setReadOnly(true);
+    m_commentView->setFont(QFontDatabase::systemFont(QFontDatabase::FixedFont));
+    m_commentBox = new QGroupBox(i18n("Comment"));
+    m_commentBox->hide();
+    QVBoxLayout *vbox = new QVBoxLayout;
+    vbox->addWidget(m_commentView);
+    m_commentBox->setLayout(vbox);
+
     setWidget(mainWidget);
     mainWidget->setLayout(m_vlayout);
 
@@ -108,8 +120,15 @@ Part::Part(QWidget *parentWidget, QObject *parent, const QVariantList& args)
     m_vlayout->setContentsMargins(0,0,0,0);
     m_vlayout->addWidget(m_splitter);
 
-    // Add widgets to the QSplitter.
-    m_splitter->addWidget(m_view);
+    // Vertical QSplitter for the file view and comment field.
+    m_commentSplitter = new QSplitter(Qt::Vertical, parentWidget);
+    m_commentSplitter->setOpaqueResize(false);
+    m_commentSplitter->addWidget(m_view);
+    m_commentSplitter->addWidget(m_commentBox);
+    m_commentSplitter->setCollapsible(0, false);
+
+    // Horizontal QSplitter for the file view and infopanel.
+    m_splitter->addWidget(m_commentSplitter);
     m_splitter->addWidget(m_infoPanel);
 
     // Read settings from config file and show/hide infoPanel.
@@ -552,6 +571,15 @@ void Part::slotLoadingFinished(KJob *job)
     m_view->header()->resizeSections(QHeaderView::ResizeToContents);
 
     updateActions();
+
+    if (!m_model->archive()->comment().isEmpty()) {
+        m_commentView->setPlainText(m_model->archive()->comment());
+        m_commentBox->show();
+        m_commentSplitter->setSizes(QList<int>() << m_view->height() * 0.6 << 1);
+    } else {
+        m_commentView->clear();
+        m_commentBox->hide();
+    }
 }
 
 void Part::setReadyGui()
@@ -1084,11 +1112,11 @@ void Part::slotShowContextMenu()
 
 void Part::displayMsgWidget(KMessageWidget::MessageType type, QString msg)
 {
-  KMessageWidget *msgWidget = new KMessageWidget;
-  msgWidget->setText(msg);
-  msgWidget->setMessageType(type);
-  m_vlayout->insertWidget(0, msgWidget);
-  msgWidget->animatedShow();
+    KMessageWidget *msgWidget = new KMessageWidget();
+    msgWidget->setText(msg);
+    msgWidget->setMessageType(type);
+    m_vlayout->insertWidget(0, msgWidget);
+    msgWidget->animatedShow();
 }
 
 } // namespace Ark

kerfuffle/archive_kerfuffle.cpp
@@ -352,6 +352,11 @@ bool Archive::isPasswordProtected()
     return m_isPasswordProtected;
 }
 
+QString Archive::comment() const
+{
+    return m_iface->comment();
+}
+
 QString Archive::subfolderName()
 {
     listIfNotListed();

kerfuffle/archive_kerfuffle.h
@@ -179,6 +179,7 @@ public:
 
     void setPassword(const QString &password);
     void enableHeaderEncryption(bool enable);
+    QString comment() const;
 
 private slots:
     void onListFinished(KJob*);
@@ -195,6 +196,7 @@ private:
     bool m_isReadOnly;
     bool m_isPasswordProtected;
     bool m_isSingleFolderArchive;
+
     QString m_subfolderName;
     qlonglong m_extractedFilesSize;
     ArchiveError m_error;

kerfuffle/archiveinterface.cpp
@@ -55,6 +55,11 @@ QString ReadOnlyArchiveInterface::filename() const
     return m_filename;
 }
 
+QString ReadOnlyArchiveInterface::comment() const
+{
+    return m_comment;
+}
+
 bool ReadOnlyArchiveInterface::isReadOnly() const
 {
     return true;

kerfuffle/archiveinterface.h
@@ -53,6 +53,11 @@ public:
     QString filename() const;
 
     /**
+     * Returns the comment of the archive.
+     */
+    QString comment() const;
+
+    /**
      * Returns whether the file can only be read.
      *
      * @return @c true  The file cannot be written.
@@ -87,7 +92,6 @@ public:
      * the user of the error condition.
      */
     virtual bool copyFiles(const QList<QVariant> & files, const QString & destinationDirectory, ExtractionOptions options) = 0;
-
     bool waitForFinishedSignal();
 
     virtual bool doKill();
@@ -119,6 +123,7 @@ protected:
     bool isHeaderEncryptionEnabled() const;
     void setCorrupt(bool isCorrupt);
     bool isCorrupt() const;
+    QString m_comment;
 
 private:
     QString m_filename;

kerfuffle/cliinterface.h
@@ -275,7 +275,6 @@ public:
     virtual void resetParsing() = 0;
     virtual ParameterList parameterList() const = 0;
     virtual bool readListLine(const QString &line) = 0;
-
     bool doKill() Q_DECL_OVERRIDE;
     bool doSuspend() Q_DECL_OVERRIDE;
     bool doResume() Q_DECL_OVERRIDE;

part/part.h
@@ -47,6 +47,8 @@ class QTemporaryDir;
 class QVBoxLayout;
 class QSignalMapper;
 class QFileSystemWatcher;
+class QGroupBox;
+class QPlainTextEdit;
 
 namespace Ark
 {
@@ -142,6 +144,9 @@ private:
     QVBoxLayout *m_vlayout;
     QSignalMapper *m_signalMapper;
     QFileSystemWatcher *m_fileWatcher;
+    QSplitter *m_commentSplitter;
+    QGroupBox *m_commentBox;
+    QPlainTextEdit *m_commentView;
 };
 
 } // namespace Ark

plugins/cli7zplugin/cliplugin.cpp
@@ -39,6 +39,7 @@ CliPlugin::CliPlugin(QObject *parent, const QVariantList & args)
         : CliInterface(parent, args)
         , m_archiveType(ArchiveType7z)
         , m_parseState(ParseStateTitle)
+        , m_linesComment(0)
 {
     qCDebug(ARK) << "Loaded cli_7z plugin";
 }
@@ -50,6 +51,7 @@ CliPlugin::~CliPlugin()
 void CliPlugin::resetParsing()
 {
     m_parseState = ParseStateTitle;
+    m_comment.clear();
 }
 
 ParameterList CliPlugin::parameterList() const
@@ -107,6 +109,7 @@ bool CliPlugin::readListLine(const QString& line)
     static const QLatin1String archiveInfoDelimiter1("--"); // 7z 9.13+
     static const QLatin1String archiveInfoDelimiter2("----"); // 7z 9.04
     static const QLatin1String entryInfoDelimiter("----------");
+    const QRegularExpression rxComment(QStringLiteral("Comment = .+$"));
 
     if (m_parseState == ParseStateTitle) {
 
@@ -157,6 +160,23 @@ bool CliPlugin::readListLine(const QString& line)
                 qCWarning(ARK) << "Unsupported archive type";
                 return false;
             }
+
+        } else if (rxComment.match(line).hasMatch()) {
+            m_parseState = ParseStateComment;
+            m_comment.append(line.section(QLatin1Char('='), 1) + QLatin1Char('\n'));
+        }
+
+    } else if (m_parseState == ParseStateComment) {
+
+        if (line == entryInfoDelimiter) {
+            m_parseState = ParseStateEntryInformation;
+            if (!m_comment.trimmed().isEmpty()) {
+                m_comment = m_comment.trimmed();
+                m_linesComment = m_comment.count(QLatin1Char('\n')) + 1;
+                qCDebug(ARK) << "Found a comment with" << m_linesComment << "lines";
+            }
+        } else {
+            m_comment.append(line + QLatin1Char('\n'));
         }
 
     } else if (m_parseState == ParseStateEntryInformation) {

commit 307a3c3295bc61dadaa2153cf029b73d03f923d4

Fix crash after a failed ListJob
Crash introduced by commit 3a22bbd. If a ListJob fails, we set a null Archive
in the ArchiveModel. So we need to check this pointer before trying to access
its comment().

part/part.cpp
@@ -587,7 +587,7 @@ void Part::slotLoadingFinished(KJob *job)
 
     updateActions();
 
-    if (!m_model->archive()->comment().isEmpty()) {
+    if (m_model->archive() && !m_model->archive()->comment().isEmpty()) {
         m_commentView->setPlainText(m_model->archive()->comment());
         m_commentBox->show();
         m_commentSplitter->setSizes(QList<int>() << m_view->height() * 0.6 << 1);