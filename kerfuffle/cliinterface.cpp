KDE/ark

commit 8fd9487d0467a5c6c0b9d5935eb4172c5fd330d7

Create temporary directories on disk and not in RAM
Currently CliInterface::copyFiles() creates a temporary directory in /tmp,
upon extraction of one or more selected files.

This does not scale well on systems where /tmp is mounted in RAM,
if the RAM is low or if the archive has huge files.

With this patch we create the temporary directory on disk.
We only need to be sure that the temp dir is created on the same disk where the final destination is
(otherwise we write each (possibly huge) file twice).

Differential Revision: D720

kerfuffle/cliinterface.cpp
@@ -271,10 +271,13 @@ bool CliInterface::copyFiles(const QVariantList &files, const QString &destinati
         }
     }
 
+    QUrl destDir = QUrl(destinationDirectory);
+    QDir::setCurrent(destDir.adjusted(QUrl::RemoveScheme).url());
+
+    QString oldCurrentDir;
     bool useTmpExtractDir = options.value(QStringLiteral("DragAndDrop")).toBool() ||
                             options.value(QStringLiteral("AlwaysUseTmpDir")).toBool();
-    QUrl destDir;
-    QTemporaryDir tmpExtractDir;
+    QTemporaryDir tmpExtractDir(QApplication::applicationName() + QLatin1Char('-'));
 
     if (useTmpExtractDir) {
         qCDebug(ARK) << "Using temporary extraction dir:" << tmpExtractDir.path();
@@ -283,11 +286,10 @@ bool CliInterface::copyFiles(const QVariantList &files, const QString &destinati
             failOperation();
             return false;
         }
+        oldCurrentDir = QDir::currentPath();
         destDir = QUrl(tmpExtractDir.path());
-    } else {
-        destDir = QUrl(destinationDirectory);
+        QDir::setCurrent(destDir.adjusted(QUrl::RemoveScheme).url());
     }
-    QDir::setCurrent(destDir.adjusted(QUrl::RemoveScheme).url());
 
     if (!runProcess(m_param.value(ExtractProgram).toStringList(), args)) {
         failOperation();
@@ -314,11 +316,13 @@ bool CliInterface::copyFiles(const QVariantList &files, const QString &destinati
                 emit finished(false);
                 return false;
             }
+            // If we don't do this, the temporary directory will not autodelete itself upon destruction.
+            QDir::setCurrent(oldCurrentDir);
         }
     }
 
     if (options.value(QStringLiteral("DragAndDrop")).toBool()) {
-        if (!moveDroppedFilesToDest(files, destinationDirectory, tmpExtractDir)) {
+        if (!moveDroppedFilesToDest(files, destinationDirectory)) {
             emit error(i18ncp("@info",
                               "Could not move the extracted file to the destination directory.",
                               "Could not move the extracted files to the destination directory.",
@@ -326,6 +330,8 @@ bool CliInterface::copyFiles(const QVariantList &files, const QString &destinati
             emit finished(false);
             return false;
         }
+        // If we don't do this, the temporary directory will not autodelete itself upon destruction.
+        QDir::setCurrent(oldCurrentDir);
     }
 
     emit finished(true);
@@ -561,7 +567,7 @@ void CliInterface::processFinished(int exitCode, QProcess::ExitStatus exitStatus
     }
 }
 
-bool CliInterface::moveDroppedFilesToDest(const QVariantList &files, const QString &finalDest, const QTemporaryDir &tmpDir)
+bool CliInterface::moveDroppedFilesToDest(const QVariantList &files, const QString &finalDest)
 {
     // Move extracted files from a QTemporaryDir to the final destination.
 
@@ -574,7 +580,7 @@ bool CliInterface::moveDroppedFilesToDest(const QVariantList &files, const QStri
     foreach (const QVariant& file, files) {
 
         QFileInfo relEntry(file.value<fileRootNodePair>().file.remove(file.value<fileRootNodePair>().rootNode));
-        QFileInfo absSourceEntry(tmpDir.path() + QLatin1Char('/') + file.value<fileRootNodePair>().file);
+        QFileInfo absSourceEntry(QDir::current().absolutePath() + QLatin1Char('/') + file.value<fileRootNodePair>().file);
         QFileInfo absDestEntry(finalDestDir.path() + QLatin1Char('/') + relEntry.filePath());
 
         if (absSourceEntry.isDir()) {

kerfuffle/cliinterface.h
@@ -374,7 +374,7 @@ private:
      */
     void writeToProcess(const QByteArray& data);
 
-    bool moveDroppedFilesToDest(const QVariantList &files, const QString &finalDest, const QTemporaryDir &tmpDir);
+    bool moveDroppedFilesToDest(const QVariantList &files, const QString &finalDest);
 
     QByteArray m_stdOutData;
     QRegularExpression m_passwordPromptPattern;

commit f409099effecba1e24f9597263552642912c1b2e

cliunarchiver: fix extraction regression
This was regressed by commit 8fd9487.

kerfuffle/cliinterface.cpp
@@ -308,7 +308,7 @@ bool CliInterface::copyFiles(const QVariantList &files, const QString &destinati
         }
 
         if (!options.value(QStringLiteral("DragAndDrop")).toBool()) {
-            if (!moveToDestination(tmpExtractDir, QDir(destinationDirectory), options[QStringLiteral("PreservePaths")].toBool())) {
+            if (!moveToDestination(QDir::current(), QDir(destinationDirectory), options[QStringLiteral("PreservePaths")].toBool())) {
                 emit error(i18ncp("@info",
                                   "Could not move the extracted file to the destination directory.",
                                   "Could not move the extracted files to the destination directory.",
@@ -498,7 +498,7 @@ bool CliInterface::runProcess(const QStringList& programNames, const QStringList
         return false;
     }
 
-    qCDebug(ARK) << "Executing" << programPath << arguments;
+    qCDebug(ARK) << "Executing" << programPath << arguments << "within directory" << QDir::currentPath();
 
     if (m_process) {
         m_process->waitForFinished();
@@ -649,9 +649,9 @@ bool CliInterface::moveDroppedFilesToDest(const QVariantList &files, const QStri
     return true;
 }
 
-bool CliInterface::moveToDestination(const QTemporaryDir &tempDir, const QDir &destDir, bool preservePaths)
+bool CliInterface::moveToDestination(const QDir &tempDir, const QDir &destDir, bool preservePaths)
 {
-    qCDebug(ARK) << "Moving extracted files to final destination" << destDir;
+    qCDebug(ARK) << "Moving extracted files from temp dir" << tempDir.path() << "to final destination" << destDir.path();
 
     bool overwriteAll = false;
     bool skipAll = false;

kerfuffle/cliinterface.h
@@ -37,7 +37,6 @@ class KProcess;
 class KPtyProcess;
 
 class QDir;
-class QTemporaryDir;
 
 namespace Kerfuffle
 {
@@ -311,7 +310,7 @@ public:
      * Move all files from @p tmpDir to @p destDir, preserving paths if @p preservePaths is true.
      * @return Whether the operation has been successfull.
      */
-    bool moveToDestination(const QTemporaryDir &tempDir, const QDir &destDir, bool preservePaths);
+    bool moveToDestination(const QDir &tempDir, const QDir &destDir, bool preservePaths);
 
 protected:
 