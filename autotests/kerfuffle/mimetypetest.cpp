0eb4f86e5d6e2860bfc908a0ccf805470aea8675
Ragnar Thomsen <rthomsen6@gmail.com>
2016-04-27 16:20:46 +0200

    Update mimetype for Debian packages
    The mimetype for Debian packages (*.deb) was updated in shared-mime-info
    1.4 from application/x-deb to application/vnd.debian.binary-package,
    resulting in Ark failing to open this archive type.
    
    This commit also adds an autotest for this mimetype.
    
    BUG: 362356
    FIXED-IN: 16.04.1

--- autotests/kerfuffle/mimetypetest.cpp
+++ autotests/kerfuffle/mimetypetest.cpp
@@ -56,6 +56,7 @@ void MimeTypeTest::testMimeTypeDetection_data()
     const QString compressedLzopTarMime = QStringLiteral("application/x-tzo");
     const QString compressedLrzipTarMime = QStringLiteral("application/x-lrzip-compressed-tar");
     const QString isoMimeType = QStringLiteral("application/x-cd-image");
+    const QString debMimeType = QStringLiteral("application/vnd.debian.binary-package");
 
     QTest::newRow("empty name") << QString() << QStringLiteral("application/octet-stream");
     QTest::newRow("tar.gz") << QFINDTESTDATA("data/simplearchive.tar.gz") << compressedGzipTarMime;
@@ -66,6 +67,7 @@ void MimeTypeTest::testMimeTypeDetection_data()
     QTest::newRow("tar.lz") << QFINDTESTDATA("data/simplearchive.tar.lz") << compressedLzipTarMime;
     QTest::newRow("tar.lzo") << QFINDTESTDATA("data/simplearchive.tar.lzo") << compressedLzopTarMime;
     QTest::newRow("tar.lrz") << QFINDTESTDATA("data/simplearchive.tar.lrz") << compressedLrzipTarMime;
+    QTest::newRow("deb") << QFINDTESTDATA("data/smallarchive.deb") << debMimeType;
 
     QTest::newRow("zip with wrong extension") << QFINDTESTDATA("data/zip_with_wrong_extension.rar") << QStringLiteral("application/zip");
     QTest::newRow("tar with special char in the extension") << QStringLiteral("foo.tar~1.gz") << compressedGzipTarMime;
