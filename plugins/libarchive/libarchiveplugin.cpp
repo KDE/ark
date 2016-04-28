765202c15f5843d9ad52480b1c5d2fbc47b30062
Elvis Angelaccio <elvis.angelaccio@kdemail.net>
2016-04-28 12:59:25 +0200

    libarchive: fix wrong order in initializer list
    This silences the -Wreorder gcc warnings

--- plugins/libarchive/libarchiveplugin.cpp
+++ plugins/libarchive/libarchiveplugin.cpp
@@ -43,10 +43,10 @@
 LibarchivePlugin::LibarchivePlugin(QObject *parent, const QVariantList & args)
     : ReadWriteArchiveInterface(parent, args)
     , m_archiveReadDisk(archive_read_disk_new())
+    , m_abortOperation(false)
     , m_cachedArchiveEntryCount(0)
     , m_emitNoEntries(false)
     , m_extractedFilesSize(0)
-    , m_abortOperation(false)
 {
     qCDebug(ARK) << "Initializing libarchive plugin";
     archive_read_disk_set_standard_lookup(m_archiveReadDisk.data());
