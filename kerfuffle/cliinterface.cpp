KDE/ark

commit 506e19405977caed98d1e7f02d1240c1d1d50d97

Fix broken corrupt queris
Ark currently always loads a corrupt archive, even if the user clicks `Don't Open`.

This is due to the `finished()` signal already emitted when the query pops up.
A simple fix is to run the query in `processFinished()`, if we detected that the
archive is corrupt.

Differential Revision: D1553

Task: T2225

kerfuffle/cliinterface.cpp
@@ -304,12 +304,22 @@ void CliInterface::processFinished(int exitCode, QProcess::ExitStatus exitStatus
         }
     }
 
-    emit progress(1.0);
-
     if (m_operationMode == Add) {
         list();
-    } else {
+    } else if (!isCorrupt()) {
+        emit progress(1.0);
         emit finished(true);
+    } else {
+        Kerfuffle::LoadCorruptQuery query(filename());
+        emit userQuery(&query);
+        query.waitForResponse();
+        if (!query.responseYes()) {
+            emit cancelled();
+            emit finished(false);
+        } else {
+            emit progress(1.0);
+            emit finished(true);
+        }
     }
 }
 
@@ -1072,14 +1082,6 @@ void CliInterface::handleLine(const QString& line)
         if (checkForErrorMessage(line, CorruptArchivePatterns)) {
             qCWarning(ARK) << "Archive corrupt";
             setCorrupt(true);
-            Kerfuffle::LoadCorruptQuery query(filename());
-            emit userQuery(&query);
-            query.waitForResponse();
-            if (!query.responseYes()) {
-                emit cancelled();
-                killProcess();
-                return;
-            }
         }
 
         if (handleFileExistsMessage(line)) {

commit 81fb4db1bfdfeae4e3dba6cb19915c2c93bc300f

Fix TestJob not emitting finished signal
Commit 506e194 introduced a regression where the finished signal didnt
get emitted for jobs other than ListJob.

kerfuffle/cliinterface.cpp
@@ -306,10 +306,7 @@ void CliInterface::processFinished(int exitCode, QProcess::ExitStatus exitStatus
 
     if (m_operationMode == Add) {
         list();
-    } else if (!isCorrupt()) {
-        emit progress(1.0);
-        emit finished(true);
-    } else {
+    } else if (m_operationMode == List && isCorrupt()) {
         Kerfuffle::LoadCorruptQuery query(filename());
         emit userQuery(&query);
         query.waitForResponse();
@@ -320,6 +317,9 @@ void CliInterface::processFinished(int exitCode, QProcess::ExitStatus exitStatus
             emit progress(1.0);
             emit finished(true);
         }
+    } else  {
+        emit progress(1.0);
+        emit finished(true);
     }
 }
 