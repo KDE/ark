d47555820c071851773177d70cd4df35201dd5b0
Elvis Angelaccio <elvis.angelaccio@kdemail.net>
2016-06-12 17:00:35 +0200

    Stop hardcoding the mimetypes in the json metadata
    This will allow us to configure the supported mimetypes at build time (e.g.
    depending on the libarchive version we are building against). We also don't
    need anymore the check for mimetypes in two different places.
    
    Differential Revision: D1822

---
+++ plugins/libarchive/kerfuffle_libarchive_readonly.json.cmake
@@ -0,0 +1,31 @@
+{
+    "KPlugin": {
+        "Authors": [
+            {
+                "Email": "henrique.pinto@kdemail.net",
+                "Name": "Henrique Pinto",
+                "Name[sr@ijekavian]": "Енрике Пинто",
+                "Name[sr@ijekavianlatin]": "Enrike Pinto",
+                "Name[sr@latin]": "Enrike Pinto",
+                "Name[sr]": "Енрике Пинто",
+                "Name[x-test]": "xxHenrique Pintoxx"
+            }
+        ],
+        "Id": "kerfuffle_libarchive_readonly",
+        "License": "BSD",
+        "MimeTypes": [
+            "@SUPPORTED_READONLY_MIMETYPES@"
+        ],
+        "Name": "kerfuffle_libarchive_readonly",
+        "Name[sv]": "Kerfuffle LibArchive skrivskyddat",
+        "Name[x-test]": "xxkerfuffle_libarchive_readonlyxx",
+        "ServiceTypes": [
+            "Kerfuffle/Plugin"
+        ],
+        "Version": "0.0.1",
+        "Website": "http://www.kde.org"
+    },
+    "X-KDE-Kerfuffle-APIRevision": 1,
+    "X-KDE-Kerfuffle-ReadWrite": false,
+    "X-KDE-Priority": 100
+}
