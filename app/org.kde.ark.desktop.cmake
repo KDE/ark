1147fd1bcb561c1a27da84af18b97733a1db84f1
l10n daemon script <scripty@kde.org>
2016-08-12 09:32:44 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- app/org.kde.ark.desktop.cmake
+++ app/org.kde.ark.desktop.cmake
@@ -178,6 +178,7 @@ Comment[nl]=Mert bestandsarchieven werken
 Comment[pl]=Pracuj z archiwami plików
 Comment[pt]=Lidar com pacotes em ficheiros
 Comment[pt_BR]=Manipulação de arquivos compactados
+Comment[ru]=Работа с архивами файлов
 Comment[sk]=Práca s archívmi súborov
 Comment[sl]=Delajte z datotečnimi arhivi
 Comment[sr]=Рад са фајловима архива
