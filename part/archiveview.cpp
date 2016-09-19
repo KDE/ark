KDE/ark

commit 1f0753660f13ad7e2a2b51aa34b7580118bcbd33

Stop crashing when dropping on empty ArchiveView
ArchiveView was accepting drops events unconditionally, overriding the
MainWindow's ones. This resulted in a crash when the ArchiveView was
empty. Now we initially disable drop events, so that we can actually
open an archive by dropping it to any position in the Ark window.

BUG: 368807
FIXED-IN: 16.08.2

part/archiveview.cpp
@@ -33,23 +33,18 @@
 ArchiveView::ArchiveView(QWidget *parent)
     : QTreeView(parent)
 {
-}
-
-void ArchiveView::setModel(QAbstractItemModel *model)
-{
-    QTreeView::setModel(model);
     setSelectionMode(QAbstractItemView::ExtendedSelection);
     setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
     setAlternatingRowColors(true);
     setAnimated(true);
     setAllColumnsShowFocus(true);
     setSortingEnabled(true);
-
-    //drag and drop
     setDragEnabled(true);
-    setAcceptDrops(true);
     setDropIndicatorShown(true);
-    setDragDropMode(QAbstractItemView::DragDrop);
+    // #368807: drops must be initially disabled, otherwise they will override the MainWindow's ones.
+    // They will be enabled in Part::slotLoadingFinished().
+    setAcceptDrops(false);
+    setDragDropMode(QAbstractItemView::NoDragDrop);
 }
 
 void ArchiveView::startDrag(Qt::DropActions supportedActions)

part/archiveview.h
@@ -34,8 +34,6 @@ public:
     virtual void dropEvent(class QDropEvent * event) Q_DECL_OVERRIDE;
     virtual void dragMoveEvent(class QDragMoveEvent * event) Q_DECL_OVERRIDE;
     virtual void startDrag(Qt::DropActions supportedActions) Q_DECL_OVERRIDE;
-
-    void setModel(QAbstractItemModel *model) Q_DECL_OVERRIDE;
 };
 
 #endif /* ARCHIVEVIEW_H */

part/part.cpp
@@ -287,8 +287,6 @@ void Part::setupView()
 
     m_view->setModel(m_model);
 
-    m_view->setSortingEnabled(true);
-
     connect(m_view->selectionModel(), &QItemSelectionModel::selectionChanged,
             this, &Part::updateActions);
     connect(m_view->selectionModel(), &QItemSelectionModel::selectionChanged,
@@ -810,6 +808,9 @@ void Part::slotLoadingFinished(KJob *job)
 
     // After loading all files, resize the columns to fit all fields
     m_view->header()->resizeSections(QHeaderView::ResizeToContents);
+    // Now we can start accepting drops in the archive view.
+    m_view->setAcceptDrops(true);
+    m_view->setDragDropMode(QAbstractItemView::DragDrop);
 
     updateActions();
 

commit e90f49e0b66ee1ae4cc421ecfc4ba36c7581c2c3

Accept drop events when creating a new archive
This fixes a small regression introduced by 1f0753660f.

part/archiveview.cpp
@@ -43,8 +43,7 @@ ArchiveView::ArchiveView(QWidget *parent)
     setDropIndicatorShown(true);
     // #368807: drops must be initially disabled, otherwise they will override the MainWindow's ones.
     // They will be enabled in Part::slotLoadingFinished().
-    setAcceptDrops(false);
-    setDragDropMode(QAbstractItemView::NoDragDrop);
+    setDropsEnabled(false);
 }
 
 void ArchiveView::startDrag(Qt::DropActions supportedActions)
@@ -58,6 +57,11 @@ void ArchiveView::startDrag(Qt::DropActions supportedActions)
     QTreeView::startDrag(supportedActions);
 }
 
+void ArchiveView::setDropsEnabled(bool enabled)
+{
+    setAcceptDrops(enabled);
+    setDragDropMode(enabled ? QAbstractItemView::DragDrop : QAbstractItemView::NoDragDrop);
+}
 
 void ArchiveView::dragEnterEvent(QDragEnterEvent * event)
 {

part/archiveview.h
@@ -34,6 +34,11 @@ public:
     virtual void dropEvent(class QDropEvent * event) Q_DECL_OVERRIDE;
     virtual void dragMoveEvent(class QDragMoveEvent * event) Q_DECL_OVERRIDE;
     virtual void startDrag(Qt::DropActions supportedActions) Q_DECL_OVERRIDE;
+
+    /**
+     * Set whether the view should accept drop events.
+     */
+    void setDropsEnabled(bool enabled);
 };
 
 #endif /* ARCHIVEVIEW_H */

part/part.cpp
@@ -674,6 +674,7 @@ bool Part::openFile()
         job->start();
     } else {
         updateActions();
+        m_view->setDropsEnabled(true);
     }
 
     m_infoPanel->setIndex(QModelIndex());
@@ -805,8 +806,7 @@ void Part::slotLoadingFinished(KJob *job)
     // After loading all files, resize the columns to fit all fields
     m_view->header()->resizeSections(QHeaderView::ResizeToContents);
     // Now we can start accepting drops in the archive view.
-    m_view->setAcceptDrops(true);
-    m_view->setDragDropMode(QAbstractItemView::DragDrop);
+    m_view->setDropsEnabled(true);
 
     updateActions();
 

part/part.h
@@ -33,6 +33,7 @@
 #include <QModelIndex>
 
 class ArchiveModel;
+class ArchiveView;
 class InfoPanel;
 
 class KAboutData;
@@ -145,7 +146,7 @@ private:
     void registerJob(KJob *job);
 
     ArchiveModel         *m_model;
-    QTreeView            *m_view;
+    ArchiveView          *m_view;
     QAction *m_previewAction;
     QAction *m_openFileAction;
     QAction *m_openFileWithAction;