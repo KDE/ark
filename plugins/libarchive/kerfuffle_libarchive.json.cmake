b345cabe3e3110970db7bdefcaa8554b79928174
l10n daemon script <scripty@kde.org>
2016-09-28 11:53:38 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- plugins/libarchive/kerfuffle_libarchive.json.cmake
+++ plugins/libarchive/kerfuffle_libarchive.json.cmake
@@ -1,79 +1,83 @@
 {
     "KPlugin": {
-        "Description": "LibArchive Plugin for Kerfuffle",
-        "Description[ca@valencia]": "Connector del LibArchive pel Kerfuffle",
-        "Description[ca]": "Connector del LibArchive pel Kerfuffle",
-        "Description[cs]": "LibArchive modul pro Kerfuffle",
-        "Description[de]": "LibArchive-Modul für Kerfuffle",
-        "Description[es]": "Complemento LibArchive para Kerfuffle",
-        "Description[fi]": "LibArchive-pakkaustuki",
-        "Description[it]": "Estensione LibArchive per Kerfuffle",
-        "Description[nl]": "LibArchive plug-in voor Kerfuffle",
-        "Description[pl]": "Wtyczka LibArchive dla Kerfuffle",
-        "Description[pt]": "'Plugin' da LibArchive para o Kerfuffle",
-        "Description[pt_BR]": "Plugin LibArchive para a Kerfuffle",
-        "Description[sk]": "LibArchive modul pre Kerfuffle",
-        "Description[sl]": "Vstavek LibArchive za Kerfuffle",
-        "Description[sr@ijekavian]": "Прикључак Либархива за Керфафл",
-        "Description[sr@ijekavianlatin]": "Priključak libarchivea za Kerfuffle",
-        "Description[sr@latin]": "Priključak libarchivea za Kerfuffle",
-        "Description[sr]": "Прикључак Либархива за Керфафл",
-        "Description[sv]": "LibArchive-insticksprogram för Kerfuffle",
-        "Description[uk]": "Додаток LibArchive для Kerfuffle",
-        "Description[x-test]": "xxLibArchive Plugin for Kerfufflexx",
-        "Description[zh_CN]": "Kerfuffle 的 LibArchive 插件",
-        "Id": "kerfuffle_libarchive",
+        "Description": "LibArchive Plugin for Kerfuffle", 
+        "Description[ca@valencia]": "Connector del LibArchive pel Kerfuffle", 
+        "Description[ca]": "Connector del LibArchive pel Kerfuffle", 
+        "Description[cs]": "LibArchive modul pro Kerfuffle", 
+        "Description[de]": "LibArchive-Modul für Kerfuffle", 
+        "Description[es]": "Complemento LibArchive para Kerfuffle", 
+        "Description[et]": "Kerfuffle'i LibArchive plugin", 
+        "Description[fi]": "LibArchive-pakkaustuki", 
+        "Description[fr]": "Module externe pour Kerfuffle", 
+        "Description[gl]": "Complemento de libarchive para Kerfuffle", 
+        "Description[it]": "Estensione LibArchive per Kerfuffle", 
+        "Description[nl]": "LibArchive plug-in voor Kerfuffle", 
+        "Description[pl]": "Wtyczka LibArchive dla Kerfuffle", 
+        "Description[pt]": "'Plugin' da LibArchive para o Kerfuffle", 
+        "Description[pt_BR]": "Plugin LibArchive para a Kerfuffle", 
+        "Description[ru]": "Модуль LibArchive для Kerfuffle", 
+        "Description[sk]": "LibArchive modul pre Kerfuffle", 
+        "Description[sl]": "Vstavek LibArchive za Kerfuffle", 
+        "Description[sr@ijekavian]": "Прикључак Либархива за Керфафл", 
+        "Description[sr@ijekavianlatin]": "Priključak libarchivea za Kerfuffle", 
+        "Description[sr@latin]": "Priključak libarchivea za Kerfuffle", 
+        "Description[sr]": "Прикључак Либархива за Керфафл", 
+        "Description[sv]": "LibArchive-insticksprogram för Kerfuffle", 
+        "Description[uk]": "Додаток LibArchive для Kerfuffle", 
+        "Description[x-test]": "xxLibArchive Plugin for Kerfufflexx", 
+        "Description[zh_CN]": "Kerfuffle 的 LibArchive 插件", 
+        "Id": "kerfuffle_libarchive", 
         "MimeTypes": [
             "@SUPPORTED_READWRITE_MIMETYPES@"
-        ],
-        "Name": "kerfuffle_libarchive",
-        "Name[sv]": "Kerfuffle LibArchive",
-        "Name[x-test]": "xxkerfuffle_libarchivexx",
+        ], 
+        "Name": "kerfuffle_libarchive", 
+        "Name[sv]": "Kerfuffle LibArchive", 
+        "Name[x-test]": "xxkerfuffle_libarchivexx", 
         "ServiceTypes": [
             "Kerfuffle/Plugin"
-        ],
+        ], 
         "Version": "@KDE_APPLICATIONS_VERSION@"
-    },
-    "X-KDE-Kerfuffle-ReadWrite": true,
-    "X-KDE-Priority": 100,
+    }, 
+    "X-KDE-Kerfuffle-ReadWrite": true, 
+    "X-KDE-Priority": 100, 
     "application/x-bzip-compressed-tar": {
-        "CompressionLevelDefault": 9,
-        "CompressionLevelMax": 9,
+        "CompressionLevelDefault": 9, 
+        "CompressionLevelMax": 9, 
         "CompressionLevelMin": 1
-    },
+    }, 
     "application/x-compressed-tar": {
-        "CompressionLevelDefault": 6,
-        "CompressionLevelMax": 9,
+        "CompressionLevelDefault": 6, 
+        "CompressionLevelMax": 9, 
         "CompressionLevelMin": 1
-    },
+    }, 
     "application/x-lrzip-compressed-tar": {
-        "CompressionLevelDefault": 1,
-        "CompressionLevelMax": 9,
+        "CompressionLevelDefault": 1, 
+        "CompressionLevelMax": 9, 
         "CompressionLevelMin": 1
-    },
-    "application/x-lzip-compressed-tar": {
-        "CompressionLevelDefault": 6,
-        "CompressionLevelMax": 9,
-        "CompressionLevelMin": 0
-    },
-    "application/x-lzma-compressed-tar": {
-        "CompressionLevelDefault": 6,
-        "CompressionLevelMax": 9,
-        "CompressionLevelMin": 0
-    },
-    "application/x-tzo": {
-        "CompressionLevelDefault": 5,
-        "CompressionLevelMax": 9,
-        "CompressionLevelMin": 1
-    },
-    "application/x-xz-compressed-tar": {
-        "CompressionLevelDefault": 6,
-        "CompressionLevelMax": 9,
-        "CompressionLevelMin": 0
-    },
+    }, 
     "application/x-lz4-compressed-tar": {
-        "CompressionLevelDefault": 1,
-        "CompressionLevelMax": 9,
+        "CompressionLevelDefault": 1, 
+        "CompressionLevelMax": 9, 
         "CompressionLevelMin": 1
+    }, 
+    "application/x-lzip-compressed-tar": {
+        "CompressionLevelDefault": 6, 
+        "CompressionLevelMax": 9, 
+        "CompressionLevelMin": 0
+    }, 
+    "application/x-lzma-compressed-tar": {
+        "CompressionLevelDefault": 6, 
+        "CompressionLevelMax": 9, 
+        "CompressionLevelMin": 0
+    }, 
+    "application/x-tzo": {
+        "CompressionLevelDefault": 5, 
+        "CompressionLevelMax": 9, 
+        "CompressionLevelMin": 1
+    }, 
+    "application/x-xz-compressed-tar": {
+        "CompressionLevelDefault": 6, 
+        "CompressionLevelMax": 9, 
+        "CompressionLevelMin": 0
     }
 }
