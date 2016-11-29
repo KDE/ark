KDE/ark

commit 1b817f61f187eda4dc14a3227e0c0430c30d808e

Sort mimetypes by comment only when necessary
The list of supported mimetypes is quite long and sorting them by
comment is a non-negligible operation.
Now we do it only when the mimetypes are shown in user-visible dialogs.

CCBUG: 372999

kerfuffle/pluginmanager.cpp
@@ -107,7 +107,7 @@ Plugin *PluginManager::preferredWritePluginFor(const QMimeType &mimeType) const
     return preferredWritePlugins.isEmpty() ? new Plugin() : preferredWritePlugins.first();
 }
 
-QStringList PluginManager::supportedMimeTypes() const
+QStringList PluginManager::supportedMimeTypes(MimeSortingMode mode) const
 {
     QSet<QString> supported;
     foreach (Plugin *plugin, availablePlugins()) {
@@ -124,10 +124,14 @@ QStringList PluginManager::supportedMimeTypes() const
         supported.remove(QStringLiteral("application/x-lz4-compressed-tar"));
     }
 
-    return sortByComment(supported);
+    if (mode == SortByComment) {
+        return sortByComment(supported);
+    }
+
+    return supported.toList();
 }
 
-QStringList PluginManager::supportedWriteMimeTypes() const
+QStringList PluginManager::supportedWriteMimeTypes(MimeSortingMode mode) const
 {
     QSet<QString> supported;
     foreach (Plugin *plugin, availableWritePlugins()) {
@@ -144,7 +148,11 @@ QStringList PluginManager::supportedWriteMimeTypes() const
         supported.remove(QStringLiteral("application/x-lz4-compressed-tar"));
     }
 
-    return sortByComment(supported);
+    if (mode == SortByComment) {
+        return sortByComment(supported);
+    }
+
+    return supported.toList();
 }
 
 QVector<Plugin*> PluginManager::filterBy(const QVector<Plugin*> &plugins, const QMimeType &mimeType) const

app/mainwindow.cpp
@@ -199,7 +199,7 @@ void MainWindow::openArchive()
     Kerfuffle::PluginManager pluginManager;
     auto dlg = new QFileDialog(this, i18nc("to open an archive", "Open Archive"));
 
-    dlg->setMimeTypeFilters(pluginManager.supportedMimeTypes());
+    dlg->setMimeTypeFilters(pluginManager.supportedMimeTypes(Kerfuffle::PluginManager::SortByComment));
     dlg->setFileMode(QFileDialog::ExistingFile);
     dlg->setAcceptMode(QFileDialog::AcceptOpen);
 

kerfuffle/createdialog.cpp
@@ -66,7 +66,7 @@ CreateDialog::CreateDialog(QWidget *parent,
     setWindowTitle(caption);
     setModal(true);
 
-    m_supportedMimeTypes = m_pluginManger.supportedWriteMimeTypes();
+    m_supportedMimeTypes = m_pluginManger.supportedWriteMimeTypes(PluginManager::SortByComment);
 
     m_vlayout = new QVBoxLayout();
     setLayout(m_vlayout);

kerfuffle/pluginmanager.h
@@ -40,6 +40,16 @@ class KERFUFFLE_EXPORT PluginManager : public QObject
     Q_OBJECT
 
 public:
+
+    /**
+     * How the list of supported mimetypes can be sorted.
+     */
+    enum MimeSortingMode
+    {
+        Unsorted,
+        SortByComment
+    };
+
     explicit PluginManager(QObject *parent = Q_NULLPTR);
 
     /**
@@ -91,14 +101,14 @@ public:
     Plugin *preferredWritePluginFor(const QMimeType &mimeType) const;
 
     /**
-     * @return The list of all mimetypes that Ark can open, sorted according to their comment.
+     * @return The list of all mimetypes that Ark can open, sorted according to @p mode.
      */
-    QStringList supportedMimeTypes() const;
+    QStringList supportedMimeTypes(MimeSortingMode mode = Unsorted) const;
 
     /**
-     * @return The list of all read-write mimetypes supported by Ark, sorted according to their comment.
+     * @return The list of all read-write mimetypes supported by Ark, sorted according to @p mode.
      */
-    QStringList supportedWriteMimeTypes() const;
+    QStringList supportedWriteMimeTypes(MimeSortingMode mode = Unsorted) const;
 
     /**
      * @return The subset of @p plugins that support either @p mimetype or a parent of @p mimetype.

commit 1c86d08166ae8e9e937b9ea95e6ad7b5ee5fa511

pluginmanager: always return valid mimetypes
This fixes a small regression introduced by 1b817f61f1.
Previously PluginManager would always return a list of *valid*
mimetypes, because sortByComment() was filtering out invalid ones.

After commit 1b817f61f1 sortByComment() is no longer unconditionally called,
so the check for valid mimetypes needs to be moved.

This was causing createdialogtest and adddialogtest to fail on the CI.

kerfuffle/pluginmanager.cpp
@@ -117,8 +117,13 @@ Plugin *PluginManager::preferredWritePluginFor(const QMimeType &mimeType) const
 QStringList PluginManager::supportedMimeTypes(MimeSortingMode mode) const
 {
     QSet<QString> supported;
+    QMimeDatabase db;
     foreach (Plugin *plugin, availablePlugins()) {
-        supported += plugin->metaData().mimeTypes().toSet();
+        foreach (const auto& mimeType, plugin->metaData().mimeTypes()) {
+            if (db.mimeTypeForName(mimeType).isValid()) {
+                supported.insert(mimeType);
+            }
+        }
     }
 
     // Remove entry for lrzipped tar if lrzip executable not found in path.
@@ -141,8 +146,13 @@ QStringList PluginManager::supportedMimeTypes(MimeSortingMode mode) const
 QStringList PluginManager::supportedWriteMimeTypes(MimeSortingMode mode) const
 {
     QSet<QString> supported;
+    QMimeDatabase db;
     foreach (Plugin *plugin, availableWritePlugins()) {
-        supported += plugin->metaData().mimeTypes().toSet();
+        foreach (const auto& mimeType, plugin->metaData().mimeTypes()) {
+            if (db.mimeTypeForName(mimeType).isValid()) {
+                supported.insert(mimeType);
+            }
+        }
     }
 
     // Remove entry for lrzipped tar if lrzip executable not found in path.
@@ -224,9 +234,7 @@ QStringList PluginManager::sortByComment(const QSet<QString> &mimeTypes)
     // Initialize the QMap to sort by comment.
     foreach (const QString &mimeType, mimeTypes) {
         QMimeType mime(QMimeDatabase().mimeTypeForName(mimeType));
-        if (mime.isValid()) {
-            map[mime.comment().toLower()] = mime.name();
-        }
+        map[mime.comment().toLower()] = mime.name();
     }
 
     // Convert to sorted QStringList.