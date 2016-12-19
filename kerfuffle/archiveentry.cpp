KDE/ark

commit 7781d6ef794a8da9afe1c837c3a7826103568add

Implement search function
An action was added to Archive menu and is also shown in the default
toolbar. The search is mediated through KRecursiveFilterProxyModel
instead of QSortFilterProxyModel because the latter does not recurse
nested models. This adds a new dependency on the KItemModels framework.

The search is always case-insensitive.

The search bar is displayed above the QTreeView and contains a close
button. An eventfilter was installed on Part to catch the escape
keypress to close the search bar.

FEATURE: 188197
FIXED-IN: 17.04.0
Differential Revision: D3573
GUI:

part/CMakeLists.txt
@@ -22,7 +22,7 @@ ki18n_wrap_ui(arkpart_PART_SRCS
 
 add_library(arkpart MODULE ${arkpart_PART_SRCS})
 
-target_link_libraries(arkpart kerfuffle KF5::Parts KF5::KIOFileWidgets)
+target_link_libraries(arkpart kerfuffle KF5::Parts KF5::KIOFileWidgets KF5::ItemModels)
 
 configure_file(
             ${CMAKE_CURRENT_SOURCE_DIR}/ark_part.desktop.cmake

part/part.cpp
@@ -51,6 +51,7 @@
 #include <KIO/StatJob>
 #include <KMessageBox>
 #include <KPluginFactory>
+#include <KRecursiveFilterProxyModel>
 #include <KRun>
 #include <KSelectAction>
 #include <KStandardGuiItem>
@@ -75,6 +76,7 @@
 #include <QFileSystemWatcher>
 #include <QGroupBox>
 #include <QPlainTextEdit>
+#include <QPushButton>
 
 using namespace Kerfuffle;
 
@@ -108,6 +110,7 @@ Part::Part(QWidget *parentWidget, QObject *parent, const QVariantList& args)
     QWidget *mainWidget = new QWidget;
     m_vlayout = new QVBoxLayout;
     m_model = new ArchiveModel(pathName, this);
+    m_filterModel = new KRecursiveFilterProxyModel(this);
     m_splitter = new QSplitter(Qt::Horizontal, parentWidget);
     m_view = new ArchiveView;
     m_infoPanel = new InfoPanel(m_model);
@@ -142,6 +145,28 @@ Part::Part(QWidget *parentWidget, QObject *parent, const QVariantList& args)
 
     setWidget(mainWidget);
     mainWidget->setLayout(m_vlayout);
+
+    // Setup search widget.
+    m_searchWidget = new QWidget(parentWidget);
+    m_searchWidget->setVisible(false);
+    m_searchWidget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Maximum);
+    QHBoxLayout *searchLayout = new QHBoxLayout;
+    searchLayout->setContentsMargins(2, 2, 2, 2);
+    m_vlayout->addWidget(m_searchWidget);
+    m_searchWidget->setLayout(searchLayout);
+    m_searchCloseButton = new QPushButton(QIcon::fromTheme(QStringLiteral("dialog-close")), QString(), m_searchWidget);
+    m_searchCloseButton->setFlat(true);
+    m_searchLineEdit = new QLineEdit(m_searchWidget);
+    m_searchLineEdit->setClearButtonEnabled(true);
+    m_searchLineEdit->setPlaceholderText(i18n("Type to search..."));
+    mainWidget->installEventFilter(this);
+    searchLayout->addWidget(m_searchCloseButton);
+    searchLayout->addWidget(m_searchLineEdit);
+    connect(m_searchCloseButton, &QPushButton::clicked, this, [=]() {
+        m_searchWidget->hide();
+        m_searchLineEdit->clear();
+    });
+    connect(m_searchLineEdit, &QLineEdit::textChanged, this, &Part::searchEdited);
 
     // Configure the QVBoxLayout and add widgets
     m_vlayout->setContentsMargins(0,0,0,0);
@@ -274,7 +299,7 @@ void Part::extractSelectedFilesTo(const QString& localPath)
     options.setDragAndDropEnabled(true);
 
     // Create and start the ExtractJob.
-    ExtractJob *job = m_model->extractFiles(filesAndRootNodesForIndexes(addChildren(m_view->selectionModel()->selectedRows())), destination, options);
+    ExtractJob *job = m_model->extractFiles(filesAndRootNodesForIndexes(addChildren(getSelectedIndexes())), destination, options);
     registerJob(job);
     connect(job, &KJob::result,
             this, &Part::slotExtractionDone);
@@ -291,7 +316,10 @@ void Part::setupView()
 {
     m_view->setContextMenuPolicy(Qt::CustomContextMenu);
 
-    m_view->setModel(m_model);
+    m_filterModel->setSourceModel(m_model);
+    m_view->setModel(m_filterModel);
+    m_filterModel->setFilterKeyColumn(0);
+    m_filterModel->setFilterCaseSensitivity(Qt::CaseInsensitive);
 
     connect(m_view->selectionModel(), &QItemSelectionModel::selectionChanged,
             this, &Part::updateActions);
@@ -428,6 +456,13 @@ void Part::setupActions()
     m_testArchiveAction->setToolTip(i18nc("@info:tooltip", "Click to test the archive for integrity"));
     connect(m_testArchiveAction, &QAction::triggered, this, &Part::slotTestArchive);
 
+    m_searchAction = actionCollection()->addAction(QStringLiteral("find_in_archive"));
+    m_searchAction->setIcon(QIcon::fromTheme(QStringLiteral("search")));
+    m_searchAction->setText(i18nc("@action:inmenu", "&Find Files"));
+    actionCollection()->setDefaultShortcut(m_searchAction, Qt::CTRL + Qt::Key_F);
+    m_searchAction->setToolTip(i18nc("@info:tooltip", "Click to search in archive"));
+    connect(m_searchAction, &QAction::triggered, this, &Part::slotShowFind);
+
     connect(m_signalMapper, static_cast<void (QSignalMapper::*)(int)>(&QSignalMapper::mapped),
             this, &Part::slotOpenEntry);
 
@@ -439,7 +474,7 @@ void Part::setupActions()
 void Part::updateActions()
 {
     bool isWritable = m_model->archive() && !m_model->archive()->isReadOnly();
-    const Archive::Entry *entry = m_model->entryForIndex(m_view->selectionModel()->currentIndex());
+    const Archive::Entry *entry = m_model->entryForIndex(m_filterModel->mapToSource(m_view->selectionModel()->currentIndex()));
     int selectedEntriesCount = m_view->selectionModel()->selectedRows().count();
 
     // We disable adding files if the archive is encrypted but the password is
@@ -507,6 +542,9 @@ void Part::updateActions()
                                    isWritable &&
                                    (selectedEntriesCount == 0 || (selectedEntriesCount == 1 && isDir)) &&
                                    (m_model->filesToMove.count() > 0 || m_model->filesToCopy.count() > 0));
+
+    m_searchAction->setEnabled(!isBusy() &&
+                               m_model->rowCount() > 0);
 
     m_commentView->setEnabled(!isBusy());
     m_commentMsgWidget->setEnabled(!isBusy());
@@ -694,7 +732,7 @@ void Part::slotQuickExtractFiles(QAction *triggeredAction)
 
         qCDebug(ARK) << "Extracting to:" << finalDestinationDirectory;
 
-        ExtractJob *job = m_model->extractFiles(filesAndRootNodesForIndexes(addChildren(m_view->selectionModel()->selectedRows())), finalDestinationDirectory, ExtractionOptions());
+        ExtractJob *job = m_model->extractFiles(filesAndRootNodesForIndexes(addChildren(getSelectedIndexes())), finalDestinationDirectory, ExtractionOptions());
         registerJob(job);
 
         connect(job, &KJob::result,
@@ -706,7 +744,16 @@ void Part::slotQuickExtractFiles(QAction *triggeredAction)
 
 void Part::selectionChanged()
 {
-    m_infoPanel->setIndexes(m_view->selectionModel()->selectedRows());
+    m_infoPanel->setIndexes(getSelectedIndexes());
+}
+
+QModelIndexList Part::getSelectedIndexes()
+{
+    QModelIndexList list;
+    foreach (const QModelIndex &i, m_view->selectionModel()->selectedRows()) {
+        list.append(m_filterModel->mapToSource(i));
+    }
+    return list;
 }
 
 bool Part::openFile()
@@ -922,7 +969,7 @@ void Part::slotOpenEntry(int mode)
 {
     qCDebug(ARK) << "Opening with mode" << mode;
 
-    QModelIndex index = m_view->selectionModel()->currentIndex();
+    QModelIndex index = m_filterModel->mapToSource(m_view->selectionModel()->currentIndex());
     Archive::Entry *entry = m_model->entryForIndex(index);
 
     // Don't open directories.
@@ -1114,7 +1161,7 @@ void Part::slotShowExtractionDialog()
         // If the user has chosen to extract only selected entries, fetch these
         // from the QTreeView.
         if (!dialog.data()->extractAllFiles()) {
-            files = filesAndRootNodesForIndexes(addChildren(m_view->selectionModel()->selectedRows()));
+            files = filesAndRootNodesForIndexes(addChildren(getSelectedIndexes()));
         }
 
         qCDebug(ARK) << "Selected " << files;
@@ -1336,7 +1383,7 @@ void Part::slotAddFiles()
     QString dialogTitle = i18nc("@title:window", "Add Files");
     const Archive::Entry *destination = Q_NULLPTR;
     if (m_view->selectionModel()->selectedRows().count() == 1) {
-        destination = m_model->entryForIndex(m_view->selectionModel()->currentIndex());
+        destination = m_model->entryForIndex(m_filterModel->mapToSource(m_view->selectionModel()->currentIndex()));
         if (destination->isDir()) {
             dialogTitle = i18nc("@title:window", "Add Files to %1", destination->fullPath());;
         } else {
@@ -1382,7 +1429,7 @@ void Part::slotEditFileName()
 
 void Part::slotCutFiles()
 {
-    QModelIndexList selectedRows = addChildren(m_view->selectionModel()->selectedRows());
+    QModelIndexList selectedRows = addChildren(getSelectedIndexes());
     m_model->filesToMove = ArchiveModel::entryMap(filesForIndexes(selectedRows));
     qCDebug(ARK) << "Entries marked to cut:" << m_model->filesToMove.values();
     m_model->filesToCopy.clear();
@@ -1398,7 +1445,7 @@ void Part::slotCutFiles()
 
 void Part::slotCopyFiles()
 {
-    m_model->filesToCopy = ArchiveModel::entryMap(filesForIndexes(addChildren(m_view->selectionModel()->selectedRows())));
+    m_model->filesToCopy = ArchiveModel::entryMap(filesForIndexes(addChildren(getSelectedIndexes())));
     qCDebug(ARK) << "Entries marked to copy:" << m_model->filesToCopy.values();
     foreach (const QModelIndex &row, m_cutIndexes) {
         m_view->dataChanged(row, row);
@@ -1414,8 +1461,8 @@ void Part::slotRenameFile(const QString &name)
         displayMsgWidget(KMessageWidget::Error, i18n("Filename can't contain slashes and can't be equal to \".\" or \"..\""));
         return;
     }
-    const Archive::Entry *entry = m_model->entryForIndex(m_view->selectionModel()->currentIndex());
-    QVector<Archive::Entry*> entriesToMove = filesForIndexes(addChildren(m_view->selectionModel()->selectedRows()));
+    const Archive::Entry *entry = m_model->entryForIndex(m_filterModel->mapToSource(m_view->selectionModel()->currentIndex()));
+    QVector<Archive::Entry*> entriesToMove = filesForIndexes(addChildren(getSelectedIndexes()));
 
     m_destination = new Archive::Entry();
     const QString &entryPath = entry->fullPath(NoTrailingSlash);
@@ -1432,7 +1479,7 @@ void Part::slotRenameFile(const QString &name)
 void Part::slotPasteFiles()
 {
     m_destination = (m_view->selectionModel()->selectedRows().count() > 0)
-                    ? m_model->entryForIndex(m_view->selectionModel()->currentIndex())
+                    ? m_model->entryForIndex(m_filterModel->mapToSource(m_view->selectionModel()->currentIndex()))
                     : Q_NULLPTR;
     if (m_destination == Q_NULLPTR) {
         m_destination = new Archive::Entry(Q_NULLPTR, QString());
@@ -1590,7 +1637,7 @@ void Part::slotDeleteFiles()
         return;
     }
 
-    DeleteJob *job = m_model->deleteFiles(filesForIndexes(addChildren(m_view->selectionModel()->selectedRows())));
+    DeleteJob *job = m_model->deleteFiles(filesForIndexes(addChildren(getSelectedIndexes())));
     connect(job, &KJob::result,
             this, &Part::slotDeleteFilesDone);
     registerJob(job);
@@ -1671,6 +1718,47 @@ void Part::slotShowContextMenu()
 
     QMenu *popup = static_cast<QMenu *>(factory()->container(QStringLiteral("context_menu"), this));
     popup->popup(QCursor::pos());
+}
+
+bool Part::eventFilter(QObject *target, QEvent *event)
+{
+    Q_UNUSED(target)
+
+    if (event->type() == QEvent::KeyPress) {
+        QKeyEvent *e = static_cast<QKeyEvent *>(event);
+        if (e->key() == Qt::Key_Escape) {
+            m_searchWidget->hide();
+            m_searchLineEdit->clear();
+            return true;
+        }
+    }
+    return false;
+}
+
+void Part::slotShowFind()
+{
+    if (m_searchWidget->isVisible()) {
+        m_searchLineEdit->selectAll();
+    } else {
+        m_searchWidget->show();
+    }
+    m_searchLineEdit->setFocus();
+}
+
+void Part::searchEdited(const QString &text)
+{
+    m_view->collapseAll();
+
+    m_filterModel->setFilterFixedString(text);
+
+    if(text.isEmpty()) {
+        m_view->collapseAll();
+        if (m_view->model()->rowCount() == 1) {
+            m_view->expandToDepth(0);
+        }
+    } else {
+        m_view->expandAll();
+    }
 }
 
 void Part::displayMsgWidget(KMessageWidget::MessageType type, const QString& msg)

part/part.h
@@ -41,9 +41,11 @@ class InfoPanel;
 class KAboutData;
 class KAbstractWidgetJobTracker;
 class KJob;
+class KRecursiveFilterProxyModel;
 class KToggleAction;
 
 class QAction;
+class QLineEdit;
 class QSplitter;
 class QTreeView;
 class QTemporaryDir;
@@ -52,6 +54,7 @@ class QSignalMapper;
 class QFileSystemWatcher;
 class QGroupBox;
 class QPlainTextEdit;
+class QPushButton;
 
 namespace Ark
 {
@@ -76,6 +79,7 @@ public:
     bool isBusy() const Q_DECL_OVERRIDE;
     KConfigSkeleton *config() const Q_DECL_OVERRIDE;
     QList<Kerfuffle::SettingsPage*> settingsPages(QWidget *parent) const Q_DECL_OVERRIDE;
+    bool eventFilter(QObject *target, QEvent *event) Q_DECL_OVERRIDE;
 
     /**
      * Validate the localFilePath() associated to this part.
@@ -156,7 +160,9 @@ private slots:
     void slotAddComment();
     void slotCommentChanged();
     void slotTestArchive();
+    void slotShowFind();
     void displayMsgWidget(KMessageWidget::MessageType type, const QString& msg);
+    void searchEdited(const QString &text);
 
 signals:
     void busy();
@@ -175,6 +181,7 @@ private:
     QVector<Kerfuffle::Archive::Entry*> filesAndRootNodesForIndexes(const QModelIndexList& list) const;
     QModelIndexList addChildren(const QModelIndexList &list) const;
     void registerJob(KJob *job);
+    QModelIndexList getSelectedIndexes();
 
     ArchiveModel         *m_model;
     ArchiveView          *m_view;
@@ -193,6 +200,7 @@ private:
     QAction *m_propertiesAction;
     QAction *m_editCommentAction;
     QAction *m_testArchiveAction;
+    QAction *m_searchAction;
     KToggleAction *m_showInfoPanelAction;
     InfoPanel            *m_infoPanel;
     QSplitter            *m_splitter;
@@ -216,6 +224,10 @@ private:
     KMessageWidget *m_commentMsgWidget;
     KMessageWidget *m_messageWidget;
     Kerfuffle::CompressionOptions m_compressionOptions;
+    KRecursiveFilterProxyModel *m_filterModel;
+    QWidget *m_searchWidget;
+    QLineEdit *m_searchLineEdit;
+    QPushButton *m_searchCloseButton;
 };
 
 } // namespace Ark

CMakeLists.txt
@@ -46,6 +46,7 @@ find_package(KF5 ${KF5_MIN_VERSION} REQUIRED COMPONENTS Archive
                                                         DocTools
                                                         I18n
                                                         IconThemes
+                                                        ItemModels
                                                         KIO
                                                         Service
                                                         Parts

part/ark_part.rc
@@ -1,5 +1,5 @@
 <!DOCTYPE kpartgui>
-<kpartgui name="ark_part" version="15">
+<kpartgui name="ark_part" version="16">
 <MenuBar>
 	<Menu name="archive">
 		<text>&amp;Archive</text>
@@ -7,8 +7,9 @@
 		<Action name="add" group="archive_edit"/>
 		<Action name="edit_comment" group="archive_edit"/>
 		<Action name="extract_all" group="archive_extract"/>
-		<Action name="properties" group="archive_props"/>
+		<Action name="find_in_archive" group="archive_props"/>
 		<Action name="test_archive" group="archive_props"/>
+		<Action name="properties" group="archive_props"/>
 	</Menu>
 	<Menu name="ark_file">
 		<text>&amp;File</text>
@@ -34,6 +35,7 @@
 	<Action name="extract"/>
 	<Action name="preview"/>
 	<Action name="openfile"/>
+	<Action name="find_in_archive"/>
 	<Separator/>
 	<Action name="add"/>
 	<Action name="delete"/>

commit 206a5eee6443680f65c1c16e53fff3f4890453de

Fix sorting in QTreeView
Commit 7781d6ef794a broke sorting in the sense that folders are no
longer sorted before files. This is due to KRecursiveFilterProxyModel's
implementation of sort() being used instead of ArchiveModel's.

We now subclass KRecursiveFilterProxyModel into ArchiveFilterSortModel
so we can re-implement lessThan() and move the relevant code from
ArchiveModel::sort().

Differential Revision: D3742

kerfuffle/archiveentry.cpp
@@ -168,16 +168,6 @@ Archive::Entry *Archive::Entry::findByPath(const QStringList &pieces, int index)
     return Q_NULLPTR;
 }
 
-void Archive::Entry::returnDirEntries(QVector<Entry*> *store)
-{
-    foreach(Entry *entry, m_entries) {
-        if (entry->isDir()) {
-            store->prepend(entry);
-            entry->returnDirEntries(store);
-        }
-    }
-}
-
 bool Archive::Entry::operator==(const Archive::Entry &right) const
 {
     return m_fullPath == right.m_fullPath;

kerfuffle/archiveentry.h
@@ -91,7 +91,6 @@ public:
     int row() const;
     Entry *find(const QString &name) const;
     Entry *findByPath(const QStringList & pieces, int index = 0) const;
-    void returnDirEntries(QVector<Entry*> *store);
 
     bool operator==(const Archive::Entry &right) const;
 

part/CMakeLists.txt
@@ -3,6 +3,7 @@ set(arkpart_PART_SRCS
 	infopanel.cpp
 	arkviewer.cpp
 	archivemodel.cpp
+        archivesortfiltermodel.cpp
 	archiveview.cpp
 	jobtracker.cpp
 	overwritedialog.cpp

part/archivemodel.cpp
@@ -41,26 +41,6 @@ using namespace Kerfuffle;
 static Archive::Entry *s_previousMatch = Q_NULLPTR;
 Q_GLOBAL_STATIC(QStringList, s_previousPieces)
 
-/**
- * Meta data related to one entry in a compressed archive.
- *
- * This is used for indexing entry properties as numbers
- * and for determining data displaying order in part's view.
- */
-enum EntryMetaDataType {
-    FullPath,            /**< The entry's file name */
-    Size,                /**< The entry's original size */
-    CompressedSize,      /**< The compressed size for the entry */
-    Permissions,         /**< The entry's permissions */
-    Owner,               /**< The user the entry belongs to */
-    Group,               /**< The user group the entry belongs to */
-    Ratio,               /**< The compression ratio for the entry */
-    CRC,                 /**< The entry's CRC */
-    Method,              /**< The compression method used on the entry */
-    Version,             /**< The archiver version needed to extract the entry */
-    Timestamp            /**< The timestamp for the current entry */
-};
-
 ArchiveModel::ArchiveModel(const QString &dbusPathName, QObject *parent)
     : QAbstractItemModel(parent)
     , m_dbusPathName(dbusPathName)
@@ -297,80 +277,6 @@ int ArchiveModel::columnCount(const QModelIndex &parent) const
 {
     Q_UNUSED(parent)
     return m_showColumns.size();
-}
-
-void ArchiveModel::sort(int column, Qt::SortOrder order)
-{
-    if (m_showColumns.size() <= column) {
-        return;
-    }
-
-    emit layoutAboutToBeChanged();
-
-    QVector<Archive::Entry*> dirEntries;
-    m_rootEntry->returnDirEntries(&dirEntries);
-    dirEntries.append(m_rootEntry.data());
-
-    foreach(Archive::Entry *dir, dirEntries) {
-        QVector < QPair<Archive::Entry*,int> > sorting(dir->entries().count());
-        for (int i = 0; i < dir->entries().count(); ++i) {
-            Archive::Entry *item = dir->entries().at(i);
-            sorting[i].first = item;
-            sorting[i].second = i;
-        }
-
-        std::stable_sort(sorting.begin(), sorting.end(), [=](const QPair<Archive::Entry*, int> &left, const QPair<Archive::Entry*, int> &right) {
-            const auto leftEntry = left.first;
-            const auto rightEntry = right.first;
-            bool isLessThan = false;  // Whether the left entry is less than the right entry.
-
-            // #234373: sort folders before files.
-            if ((leftEntry->isDir()) && (!rightEntry->isDir())) {
-                isLessThan = true;
-            } else if ((!leftEntry->isDir()) && (rightEntry->isDir())) {
-                isLessThan = false;
-            } else {
-                const QVariant leftEntryMetaData = leftEntry->property(m_propertiesMap[column]);
-                const QVariant rightEntryMetaData = rightEntry->property(m_propertiesMap[column]);
-
-                switch (column) {
-                case FullPath:
-                    isLessThan = leftEntry->name() < rightEntry->name();
-                    break;
-                case Size:
-                case CompressedSize:
-                    isLessThan = leftEntryMetaData.toInt() < rightEntryMetaData.toInt();
-                    break;
-                default:
-                    isLessThan = leftEntryMetaData.toString() < rightEntryMetaData.toString();
-                    break;
-                }
-            }
-
-            if (order == Qt::AscendingOrder) {
-                return isLessThan;
-            }
-            // Descending order.
-            return !isLessThan;
-        });
-
-        QModelIndexList fromIndexes;
-        QModelIndexList toIndexes;
-        for (int r = 0; r < sorting.count(); ++r) {
-            Archive::Entry *item = sorting.at(r).first;
-            toIndexes.append(createIndex(r, 0, item));
-            fromIndexes.append(createIndex(sorting.at(r).second, 0, sorting.at(r).first));
-            dir->setEntryAt(r, sorting.at(r).first);
-        }
-
-        changePersistentIndexList(fromIndexes, toIndexes);
-
-        emit dataChanged(
-            index(0, 0, indexForEntry(dir)),
-            index(dir->entries().size() - 1, 0, indexForEntry(dir)));
-    }
-
-    emit layoutChanged();
 }
 
 Qt::DropActions ArchiveModel::supportedDropActions() const
@@ -1004,4 +910,14 @@ qulonglong ArchiveModel::numberOfFolders() const
 qulonglong ArchiveModel::uncompressedSize() const
 {
     return m_uncompressedSize;
+}
+
+QList<int> ArchiveModel::shownColumns() const
+{
+    return m_showColumns;
+}
+
+QMap<int, QByteArray> ArchiveModel::propertiesMap() const
+{
+    return m_propertiesMap;
 }

part/archivemodel.h
@@ -37,6 +37,26 @@ namespace Kerfuffle
     class Query;
 }
 
+/**
+ * Meta data related to one entry in a compressed archive.
+ *
+ * This is used for indexing entry properties as numbers
+ * and for determining data displaying order in part's view.
+ */
+enum EntryMetaDataType {
+    FullPath,            /**< The entry's file name */
+    Size,                /**< The entry's original size */
+    CompressedSize,      /**< The compressed size for the entry */
+    Permissions,         /**< The entry's permissions */
+    Owner,               /**< The user the entry belongs to */
+    Group,               /**< The user group the entry belongs to */
+    Ratio,               /**< The compression ratio for the entry */
+    CRC,                 /**< The entry's CRC */
+    Method,              /**< The compression method used on the entry */
+    Version,             /**< The archiver version needed to extract the entry */
+    Timestamp            /**< The timestamp for the current entry */
+};
+
 class ArchiveModel: public QAbstractItemModel
 {
     Q_OBJECT
@@ -54,8 +74,6 @@ public:
     int rowCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
     int columnCount(const QModelIndex &parent = QModelIndex()) const Q_DECL_OVERRIDE;
 
-    virtual void sort(int column, Qt::SortOrder order = Qt::AscendingOrder) Q_DECL_OVERRIDE;
-
     //drag and drop related
     Qt::DropActions supportedDropActions() const Q_DECL_OVERRIDE;
     QStringList mimeTypes() const Q_DECL_OVERRIDE;
@@ -66,6 +84,9 @@ public:
     void createEmptyArchive(const QString &path, const QString &mimeType, QObject *parent);
     KJob* loadArchive(const QString &path, const QString &mimeType, QObject *parent);
     Kerfuffle::Archive *archive() const;
+
+    QList<int> shownColumns() const;
+    QMap<int, QByteArray> propertiesMap() const;
 
     Archive::Entry *entryForIndex(const QModelIndex &index);
     int childCount(const QModelIndex &index, int &dirs, int &files) const;

part/archivesortfiltermodel.cpp
@@ -1,0 +1,66 @@
+/*
+ * ark -- archiver for the KDE project
+ *
+ * Copyright (c) 2016 Ragnar Thomsen <rthomsen6@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+#include "archivesortfiltermodel.h"
+#include "archiveentry.h"
+#include "archivemodel.h"
+
+using namespace Kerfuffle;
+
+ArchiveSortFilterModel::ArchiveSortFilterModel(QObject *parent)
+    : KRecursiveFilterProxyModel(parent)
+{
+}
+
+ArchiveSortFilterModel::~ArchiveSortFilterModel()
+{
+}
+
+bool ArchiveSortFilterModel::lessThan(const QModelIndex &leftIndex,
+                                      const QModelIndex &rightIndex) const
+{
+    ArchiveModel *srcModel = qobject_cast<ArchiveModel*>(sourceModel());
+    const int col = srcModel->shownColumns().at(leftIndex.column());
+    const QByteArray property = srcModel->propertiesMap()[col];
+
+    const Archive::Entry *left = srcModel->entryForIndex(leftIndex);
+    const Archive::Entry *right = srcModel->entryForIndex(rightIndex);
+
+    if (left->isDir() && !right->isDir()) {
+        return true;
+    } else if (!left->isDir() && right->isDir()) {
+        return false;
+    } else {
+        switch (col) {
+        case Size:
+        case CompressedSize:
+            if (left->property(property).toULongLong() < right->property(property).toULongLong()) {
+                return true;
+            }
+            break;
+        default:
+            if (left->property(property).toString() < right->property(property).toString()) {
+                return true;
+            }
+        }
+    }
+    return false;
+}

part/archivesortfiltermodel.h
@@ -1,0 +1,37 @@
+/*
+ * ark -- archiver for the KDE project
+ *
+ * Copyright (c) 2016 Ragnar Thomsen <rthomsen6@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+#ifndef ARCHIVESORTFILTERMODEL_H
+#define ARCHIVESORTFILTERMODEL_H
+
+#include <KRecursiveFilterProxyModel>
+
+class ArchiveSortFilterModel: public KRecursiveFilterProxyModel
+{
+    Q_OBJECT
+
+public:
+    explicit ArchiveSortFilterModel(QObject *parent = 0);
+    ~ArchiveSortFilterModel();
+
+    bool lessThan(const QModelIndex &leftIndex, const QModelIndex &rightIndex) const Q_DECL_OVERRIDE;
+};
+
+#endif // ARCHIVESORTFILTERMODEL_H

part/part.cpp
@@ -28,6 +28,7 @@
 #include "overwritedialog.h"
 #include "archiveformat.h"
 #include "archivemodel.h"
+#include "archivesortfiltermodel.h"
 #include "archiveview.h"
 #include "arkviewer.h"
 #include "dnddbusinterfaceadaptor.h"
@@ -52,7 +53,6 @@
 #include <KIO/StatJob>
 #include <KMessageBox>
 #include <KPluginFactory>
-#include <KRecursiveFilterProxyModel>
 #include <KRun>
 #include <KSelectAction>
 #include <KStandardGuiItem>
@@ -111,7 +111,7 @@ Part::Part(QWidget *parentWidget, QObject *parent, const QVariantList& args)
     QWidget *mainWidget = new QWidget;
     m_vlayout = new QVBoxLayout;
     m_model = new ArchiveModel(pathName, this);
-    m_filterModel = new KRecursiveFilterProxyModel(this);
+    m_filterModel = new ArchiveSortFilterModel(this);
     m_splitter = new QSplitter(Qt::Horizontal, parentWidget);
     m_view = new ArchiveView;
     m_infoPanel = new InfoPanel(m_model);

part/part.h
@@ -35,13 +35,13 @@
 #include <QModelIndex>
 
 class ArchiveModel;
+class ArchiveSortFilterModel;
 class ArchiveView;
 class InfoPanel;
 
 class KAboutData;
 class KAbstractWidgetJobTracker;
 class KJob;
-class KRecursiveFilterProxyModel;
 class KToggleAction;
 
 class QAction;
@@ -224,7 +224,7 @@ private:
     KMessageWidget *m_commentMsgWidget;
     KMessageWidget *m_messageWidget;
     Kerfuffle::CompressionOptions m_compressionOptions;
-    KRecursiveFilterProxyModel *m_filterModel;
+    ArchiveSortFilterModel *m_filterModel;
     QWidget *m_searchWidget;
     QLineEdit *m_searchLineEdit;
     QPushButton *m_searchCloseButton;