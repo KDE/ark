561ba8ee8254b81ff6815d653829d20c82a52c0d
l10n daemon script <scripty@kde.org>
2017-03-18 04:22:18 +0100

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- plugins/libzipplugin/kerfuffle_libzip.json.cmake
+++ plugins/libzipplugin/kerfuffle_libzip.json.cmake
@@ -1,11 +1,25 @@
 {
     "KPlugin": {
         "Description": "Full support for the zip archive format", 
+        "Description[ca]": "Implementació completa del format d'arxiu ZIP", 
+        "Description[es]": "Uso total del formato de archivo comprimido zip", 
+        "Description[nl]": "Volledige ondersteuning voor het zip-archiefformaat", 
+        "Description[pt]": "Suporte total para o formato de pacotes ZIP", 
+        "Description[sv]": "Fullt stöd för arkivformatet zip", 
+        "Description[uk]": "Повноцінна підтримка архівів у форматі zip", 
+        "Description[x-test]": "xxFull support for the zip archive formatxx", 
         "Id": "kerfuffle_libzip", 
         "MimeTypes": [
             "@SUPPORTED_MIMETYPES@"
         ], 
         "Name": "Libzip plugin", 
+        "Name[ca]": "Connector del Libzip", 
+        "Name[es]": "Complemento Libzip", 
+        "Name[nl]": "Libzip-plug-in", 
+        "Name[pt]": "'Plugin' da Libzip", 
+        "Name[sv]": "Libzip-insticksprogram", 
+        "Name[uk]": "Додаток libzip", 
+        "Name[x-test]": "xxLibzip pluginxx", 
         "ServiceTypes": [
             "Kerfuffle/Plugin"
         ], 
