KDE/ark

commit 20c09f3c434b9487f98f53459776ec4b6332c2e4

Don't add invalid URLs to the recent files
Part::openFile() used to return false if the URL of the part was not a
valid archive. This is no longer the case as nowadays we run an async
LoadJob. So MainWindow cannot use the return value of openFile() to
choose whether to add the URL to the recent files. Instead, we connect
to the part's completed() signal which is emitted only if the LoadJob
ends without errors.

part/part.cpp
@@ -776,11 +776,12 @@ bool Part::openFile()
 
     if (creatingNewArchive) {
         createArchive();
-    } else {
-        loadArchive();
+        return true;
     }
 
-    return true;
+    loadArchive();
+    // Loading is async, we don't know yet whether we got a valid archive.
+    return false;
 }
 
 bool Part::saveFile()
@@ -891,6 +892,8 @@ void Part::slotLoadingFinished(KJob *job)
 
             emit setWindowCaption(QString());
         }
+    } else {
+        emit completed();
     }
 
     m_view->sortByColumn(0, Qt::AscendingOrder);

app/mainwindow.cpp
@@ -164,6 +164,7 @@ bool MainWindow::loadPart()
     // #365200: this will disable m_recentFilesAction, while openUrl() will enable it.
     // So updateActions() needs to be called after openUrl() returns.
     connect(m_part, SIGNAL(busy()), this, SLOT(updateActions()), Qt::QueuedConnection);
+    connect(m_part, static_cast<void (KParts::ReadOnlyPart::*)()>(&KParts::ReadOnlyPart::completed), this, &MainWindow::addPartUrl);
 
     return true;
 }
@@ -219,15 +220,12 @@ void MainWindow::openArchive()
 
 void MainWindow::openUrl(const QUrl& url)
 {
-    if (!url.isEmpty()) {
-        m_part->setArguments(m_openArgs);
-
-        if (m_part->openUrl(url)) {
-            m_recentFilesAction->addUrl(url);
-        } else {
-            m_recentFilesAction->removeUrl(url);
-        }
+    if (url.isEmpty()) {
+        return;
     }
+
+    m_part->setArguments(m_openArgs);
+    m_part->openUrl(url);
 }
 
 void MainWindow::setShowExtractDialog(bool option)
@@ -284,6 +282,11 @@ void MainWindow::writeSettings()
     Interface *iface = qobject_cast<Interface*>(m_part);
     Q_ASSERT(iface);
     iface->config()->save();
+}
+
+void MainWindow::addPartUrl()
+{
+    m_recentFilesAction->addUrl(m_part->url());
 }
 
 void MainWindow::newArchive()

app/mainwindow.h
@@ -58,6 +58,7 @@ private slots:
     void quit();
     void showSettings();
     void writeSettings();
+    void addPartUrl();
 
 private:
     void setupActions();

commit 518350c9abce90b607a7f3ada95578388e74e9c2

part: update window title whenever the URL changes
This fixes a small regression in commit 20c09f3c43.
A ReadOnlyPart emits `completed()` if `openUrl()` returns true, so we
lost a call to `setFileNameFromArchive()`.
We fix that by connecting this slot to the `urlChanged()` signal.

Tested by opening some archive and then an header-encrypted archive, and
making sure the window title had the name of the second archive while the
password dialog was open.

part/part.cpp
@@ -213,6 +213,8 @@ Part::Part(QWidget *parentWidget, QObject *parent, const QVariantList& args)
             this, &Part::setBusyGui);
     connect(this, &Part::ready,
             this, &Part::setReadyGui);
+    connect(this, &KParts::ReadOnlyPart::urlChanged,
+            this, &Part::setFileNameFromArchive);
     connect(this, static_cast<void (KParts::ReadOnlyPart::*)()>(&KParts::ReadOnlyPart::completed),
             this, &Part::setFileNameFromArchive);
     connect(ArkSettings::self(), &KCoreConfigSkeleton::configChanged, this, &Part::updateActions);