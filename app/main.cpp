b3db87e0e057597bee955764ca3219ed70845181
Ragnar Thomsen <rthomsen6@gmail.com>
2018-11-06 19:37:52 +0100

    Update copyright year
    GIT_SILENT

--- app/main.cpp
+++ app/main.cpp
@@ -94,7 +94,7 @@ int main(int argc, char **argv)
                          QStringLiteral(ARK_VERSION_STRING),
                          i18n("KDE Archiving tool"),
                          KAboutLicense::GPL,
-                         i18n("(c) 1997-2017, The Ark Developers"),
+                         i18n("(c) 1997-2018, The Ark Developers"),
                          QString(),
                          QStringLiteral("http://utils.kde.org/projects/ark")
     );
