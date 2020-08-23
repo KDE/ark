318ce861a5086f1cbef5a4846f85501defdc0041
Elvis Angelaccio <elvis.angelaccio@kde.org>
2020-08-23 23:22:09 +0200

    libarchive: drop dead code
    It's been commented since 68e58f5d8c8074fb from 13 years ago.

--- plugins/libarchive/libarchiveplugin.cpp
+++ plugins/libarchive/libarchiveplugin.cpp
@@ -512,17 +512,6 @@ int LibarchivePlugin::extractionFlags() const
     int result = ARCHIVE_EXTRACT_TIME;
     result |= ARCHIVE_EXTRACT_SECURE_NODOTDOT;
 
-    // TODO: Don't use arksettings here
-    /*if ( ArkSettings::preservePerms() )
-    {
-        result &= ARCHIVE_EXTRACT_PERM;
-    }
-
-    if ( !ArkSettings::extractOverwrite() )
-    {
-        result &= ARCHIVE_EXTRACT_NO_OVERWRITE;
-    }*/
-
     return result;
 }
 
