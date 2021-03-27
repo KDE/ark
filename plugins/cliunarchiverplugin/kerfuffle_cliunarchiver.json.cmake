4ce7b9102350383cab35e50cc8da30b6c52a7e1b
l10n daemon script <scripty@kde.org>
2021-03-27 09:27:16 +0100

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- plugins/cliunarchiverplugin/kerfuffle_cliunarchiver.json.cmake
+++ plugins/cliunarchiverplugin/kerfuffle_cliunarchiver.json.cmake
@@ -2,6 +2,7 @@
     "KPlugin": {
         "Description": "Open and extract RAR and LHA archives",
         "Description[ca]": "Obre i extreu arxius RAR i LHA",
+        "Description[cs]": "Otevřít a rozbalit archivy RAR a LHA",
         "Description[es]": "Abrir y extraer archivos RAR y LHA",
         "Description[fr]": "Ouvre et extrait les archives « RAR » et « LHA »",
         "Description[ia]": "Aperir e extraher archivos RAR e LHA ",
