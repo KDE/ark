ff217050fee9241bee0e7fc1f71a0c8adfd5b19c
Nate Graham <nate@kde.org>
2021-05-04 09:32:22 -0600

    Revert "Add new "page" instead of UI with disabled widgets"
    This reverts commit 632d2617bbb410741664b1560ee687611d7012f3.
    
    I accdentally merged this instead of clicking the thumbs up button. My
    apologies. :(

--- app/mainwindow.h
+++ app/mainwindow.h
@@ -3,7 +3,6 @@
  *
  * Copyright (C) 2007 Henrique Pinto <henrique.pinto@kdemail.net>
  * Copyright (C) 2008 Harald Hvaal <haraldhv@stud.ntnu.no>
- * Copyright (C) 2021 Jiří Wolker <woljiri@gmail.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -25,9 +24,6 @@
 
 #include <KParts/MainWindow>
 #include <KParts/OpenUrlArguments>
-#include <QStackedWidget>
-
-#include "welcomescreen.h"
 
 namespace KParts
 {
@@ -40,7 +36,7 @@ class MainWindow: public KParts::MainWindow
 {
     Q_OBJECT
 public:
-    MainWindow(QWidget* = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
     bool loadPart();
 
@@ -51,9 +47,6 @@ public:
 public Q_SLOTS:
     void openUrl(const QUrl &url);
     void setShowExtractDialog(bool);
-
-    void showWelcomeScreen();
-    void hideWelcomeScreen();
 
 protected:
     void closeEvent(QCloseEvent *event) override;
@@ -75,8 +68,6 @@ private:
     QAction               *m_openAction;
     QAction               *m_newAction;
     KParts::OpenUrlArguments m_openArgs;
-    WelcomeScreen         *m_welcomeScreen;
-    QStackedWidget        *m_windowContents;
 };
 
 #endif // MAINWINDOW_H
