9540de6fdaacf227917b7256684feb4f6d0a2ad1
Friedrich W. H. Kossebau <kossebau@kde.org>
2021-07-07 14:34:18 +0200

    Add support for single files compressed with zstd
    BUG: 434540

---
+++ plugins/libsinglefileplugin/kerfuffle_libzstd.json.cmake
@@ -0,0 +1,12 @@
+{
+    "KPlugin": {
+        "Description": "Open and extract single files compressed with the zstd algorithm",
+        "Id": "kerfuffle_libzstd",
+        "MimeTypes": [
+            "@SUPPORTED_MIMETYPES@"
+        ],
+        "Name": "Zstd plugin",
+        "Version": "@RELEASE_SERVICE_VERSION@"
+    },
+    "X-KDE-Priority": 100
+}
