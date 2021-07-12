6f262cdd5b26086d81faf8fc33e3046f8e70f972
l10n daemon script <scripty@kde.org>
2021-07-12 01:16:57 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- plugins/libsinglefileplugin/kerfuffle_libzstd.json.cmake
+++ plugins/libsinglefileplugin/kerfuffle_libzstd.json.cmake
@@ -1,11 +1,31 @@
 {
     "KPlugin": {
         "Description": "Open and extract single files compressed with the zstd algorithm",
+        "Description[ca]": "Obre i extreu fitxers individuals comprimits amb l'algorisme «zstd»",
+        "Description[en_GB]": "Open and extract single files compressed with the zstd algorithm",
+        "Description[es]": "Abrir y extraer archivos individuales comprimidos con el algoritmo zstd",
+        "Description[eu]": "Ireki eta erauzi zstd algoritmoarekin konprimatutako fitxategi bakunak",
+        "Description[nl]": "Open en pak uit losse bestanden gecomprimeerd met het zstd-algoritme",
+        "Description[pl]": "Otwiera i wypakowuje pojedyncze pliki spakowane algorytmem zstd",
+        "Description[pt]": "Abrir e extrair ficheiros individuais comprimidos com o algoritmo ZSTD",
+        "Description[sv]": "Öppna och packa upp enstaka filer komprimerade med zstd-algoritmen",
+        "Description[uk]": "Здатен відкривати і видобувати дані із окремих файлів, стиснених за допомогою алгоритму zstd",
+        "Description[x-test]": "xxOpen and extract single files compressed with the zstd algorithmxx",
         "Id": "kerfuffle_libzstd",
         "MimeTypes": [
             "@SUPPORTED_MIMETYPES@"
         ],
         "Name": "Zstd plugin",
+        "Name[ca]": "Connector del Zstd",
+        "Name[en_GB]": "Zstd plugin",
+        "Name[es]": "Complemento Zstd",
+        "Name[eu]": "Zstd plugina",
+        "Name[nl]": "Zstd-plug-in",
+        "Name[pl]": "Wtyczka Zstd",
+        "Name[pt]": "'Plugin' do Zstd",
+        "Name[sv]": "Zstd-insticksprogram",
+        "Name[uk]": "Додаток zstd",
+        "Name[x-test]": "xxZstd pluginxx",
         "Version": "@RELEASE_SERVICE_VERSION@"
     },
     "X-KDE-Priority": 100
