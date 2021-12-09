75bf57578fa1042c5cf47404934b62f25cf12716
Kai Uwe Broulik <kde@privat.broulik.de>
2021-12-09 23:32:27 +0100

    Mark LoadJob as transient
    Avoids a "Loading archive (finished)" remaining in notification history

--- kerfuffle/jobs.cpp
+++ kerfuffle/jobs.cpp
@@ -232,6 +232,8 @@ LoadJob::LoadJob(Archive *archive, ReadOnlyArchiveInterface *interface)
     , m_dirCount(0)
     , m_filesCount(0)
 {
+    // Don't show "finished" notification when finished
+    setProperty("transientProgressReporting", true);
     qCDebug(ARK) << "Created job instance";
     connect(this, &LoadJob::newEntry, this, &LoadJob::onNewEntry);
 }
