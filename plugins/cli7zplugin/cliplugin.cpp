6187ddc4bed6b9503406c3b68ddfeec0358e7d99
Hanabishi Recca <irecca.kun@gmail.com>
2022-09-20 18:59:18 +0500

    Fix incompatibility with original 7-Zip
    It is wrong to expect strictly z here, the letter can be different and depends on 7-Zip build type.
    
    BUG: 456797
    FIXED-IN: 22.08.2

--- plugins/cli7zplugin/cliplugin.cpp
+++ plugins/cli7zplugin/cliplugin.cpp
@@ -132,7 +132,7 @@ bool CliPlugin::readListLine(const QString& line)
     }
 
     const QRegularExpression rxVersionLine(QStringLiteral("^p7zip Version ([\\d\\.]+) .*$"));
-    const QRegularExpression rxVersionLine7z(QStringLiteral("^7-Zip \\(z\\) ([\\d\\.]+) .*$"));
+    const QRegularExpression rxVersionLine7z(QStringLiteral("^7-Zip \\(\\w\\) ([\\d\\.]+) .*$"));
     QRegularExpressionMatch matchVersion;
 
     switch (m_parseState) {
