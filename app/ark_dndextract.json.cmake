a99a69576b75f5d4c5fedeac82f67b31f272c8f9
l10n daemon script <scripty@kde.org>
2023-07-14 01:48:24 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- app/ark_dndextract.json.cmake
+++ app/ark_dndextract.json.cmake
@@ -3,6 +3,14 @@
         "MimeTypes": [
             "@SUPPORTED_ARK_MIMETYPES_JSON@"
         ],
-        "Name": "Ark Extract Here"
+        "Name": "Ark Extract Here",
+        "Name[ca]": "Extracció de l'Ark aquí",
+        "Name[ia]": "Ark Extrahe Hic",
+        "Name[nl]": "Ark: hier uitpakken",
+        "Name[ru]": "Распаковать в эту папку",
+        "Name[sl]": "Arkov ekstrakt tukaj",
+        "Name[tr]": "Ark Buraya Çıkar",
+        "Name[uk]": "Розпакувати сюди за допомогою Ark",
+        "Name[x-test]": "xxArk Extract Herexx"
     }
 }
--- part/ark_part.json.cmake
+++ part/ark_part.json.cmake
@@ -1,12 +1,27 @@
 {
     "KPlugin": {
         "Description": "Archive Handling Tool",
+        "Description[ca]": "Eina de gestió d'arxius",
+        "Description[ia]": "Instrumento de manear archivo",
+        "Description[nl]": "Hulpprogramma voor het beheren van archieven",
+        "Description[sl]": "Orodje za upravljanje arhivov",
+        "Description[tr]": "Arşiv İşleme Aracı",
+        "Description[uk]": "Інструмент для роботи з архівами",
+        "Description[x-test]": "xxArchive Handling Toolxx",
         "Icon": "ark",
         "License": "GPL v2+",
         "MimeTypes": [
             "@SUPPORTED_ARK_MIMETYPES_JSON@"
         ],
         "Name": "Archiver",
+        "Name[ca]": "Arxivador",
+        "Name[ia]": "Archivaor",
+        "Name[nl]": "Archiefgereedschap",
+        "Name[ru]": "Архиватор",
+        "Name[sl]": "Arhivar",
+        "Name[tr]": "Arşivleyici",
+        "Name[uk]": "Архіватор",
+        "Name[x-test]": "xxArchiverxx",
         "ServiceTypes": [
             "KParts/ReadOnlyPart",
             "Browser/View"
