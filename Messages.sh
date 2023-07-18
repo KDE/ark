59258700be34b59b410c75b8f5c6e402ad40981c
Albert Astals Cid <aacid@kde.org>
2023-07-18 09:20:18 +0200

    Make sure welcomeview messages are extracted
    BUGS: 472341
    (cherry picked from commit ef9fe35998e4d3a605f30869f13739aef7646310)

--- Messages.sh
+++ Messages.sh
@@ -2,5 +2,5 @@
 $EXTRACTRC $(find . -name '*.rc') >> rc.cpp || exit 11
 $EXTRACTRC $(find . -name '*.ui') >> rc.cpp || exit 12
 $EXTRACTRC $(find . -name '*.kcfg') >> rc.cpp
-$XGETTEXT app/*.cpp kerfuffle/*.cpp part/*.cpp plugins/*/*.cpp rc.cpp -o $podir/ark.pot
+$XGETTEXT $(find app kerfuffle part plugins -name '*.cpp') rc.cpp -o $podir/ark.pot
 rm -f rc.cpp
