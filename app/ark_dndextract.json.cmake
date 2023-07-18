c19f74fa78393f79def56d5c66b6608f74f1d598
l10n daemon script <scripty@kde.org>
2023-07-18 04:06:57 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- app/ark_dndextract.json.cmake
+++ app/ark_dndextract.json.cmake
@@ -3,20 +3,6 @@
         "MimeTypes": [
             "@SUPPORTED_ARK_MIMETYPES_JSON@"
         ],
-        "Name": "Ark Extract Here",
-        "Name[ca@valencia]": "Extracció d'Ark ací",
-        "Name[ca]": "Extracció de l'Ark aquí",
-        "Name[eo]": "Ark-Extrakti ĉi tie",
-        "Name[es]": "Ark - Extraer aquí",
-        "Name[eu]": "Ark Erauzi hemen",
-        "Name[ia]": "Ark Extrahe Hic",
-        "Name[ka]": "Ark აქ გაშლა",
-        "Name[nl]": "Ark: hier uitpakken",
-        "Name[pt]": "Ark - Extrair Aqui",
-        "Name[ru]": "Распаковать в эту папку",
-        "Name[sl]": "Arkov ekstrakt tukaj",
-        "Name[tr]": "Ark Buraya Çıkar",
-        "Name[uk]": "Розпакувати сюди за допомогою Ark",
-        "Name[x-test]": "xxArk Extract Herexx"
+        "Name": "Ark Extract Here"
     }
 }
