2953077f3bd65848b61714ba83d2953e22decde4
Ilya Pominov <ipominov@astralinux.ru>
2024-03-19 20:41:55 +0300

    clirar: expand the list of messages about incorrect password
    The unrar utility changed the message about incorrect password when
    unpacking an encrypted rar archive.
    
    New message for unrar 6.11 has format:
    "Incorrect password for filename"
    
    STEPS TO REPRODUCE:
    1. Try extract rar archive with clirar plugin
    2. Enter incorrect password
    
    EXPECTED RESULT:
    Ark show error message about incorrect password
    
    OBSERVED RESULT:
    Ark show message "Extracting Files (Finished)"

--- plugins/clirarplugin/cliplugin.cpp
+++ plugins/clirarplugin/cliplugin.cpp
@@ -562,7 +562,8 @@ bool CliPlugin::isPasswordPrompt(const QString &line)
 
 bool CliPlugin::isWrongPasswordMsg(const QString &line)
 {
-    return (line.contains(QLatin1String("password incorrect")) || line.contains(QLatin1String("wrong password")));
+    return line.contains(QLatin1String("password incorrect")) || line.contains(QLatin1String("wrong password"))
+        || line.contains(QLatin1String("Incorrect password"));
 }
 
 bool CliPlugin::isCorruptArchiveMsg(const QString &line)
