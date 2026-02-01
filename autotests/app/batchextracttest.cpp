0fae5585314bb24ef46208a8431dcbecb3a3fcf0
Rafał Lichwała <rafal@siliconet.pl>
2026-02-01 19:26:32 +0100

    batchextracttest: modify test to cover existing folder use-case
    Replace deprecated QDirIterator with QDirListing

--- autotests/app/batchextracttest.cpp
+++ autotests/app/batchextracttest.cpp
@@ -6,7 +6,6 @@
 
 #include "batchextract.h"
 
-#include <QDirIterator>
 #include <QTest>
 
 class BatchExtractTest : public QObject
@@ -37,20 +36,28 @@ void BatchExtractTest::testBatchExtraction_data()
     // Expected numbers of entries (files + folders) in the temporary extraction folder.
     // This is the number of entries in the archive (+ 1, if the autosubfolder is expected).
     QTest::addColumn<int>("expectedExtractedEntriesCount");
+    QTest::addColumn<bool>("createExistingDir");
+    QTest::addColumn<int>("expectedTopLevelEntriesCount");
 
-    QTest::newRow("extract the whole simple%archive.tar.gz (bug #365798)") << QFINDTESTDATA("data/simple%archive.tar.gz") << true << 5;
+    QTest::newRow("extract the whole simple%archive.tar.gz (bug #365798), empty destination")
+        << QFINDTESTDATA("data/simple%archive.tar.gz") << true << 5 << false << 1;
 
-    QTest::newRow("single-folder, no autosubfolder") << QFINDTESTDATA("../kerfuffle/data/one_toplevel_folder.zip") << false << 9;
+    QTest::newRow("single-folder, no autosubfolder, empty destination")
+        << QFINDTESTDATA("../kerfuffle/data/one_toplevel_folder.zip") << false << 9 << false << 1;
 
-    QTest::newRow("single-folder, autosubfolder") << QFINDTESTDATA("../kerfuffle/data/one_toplevel_folder.zip") << true << 9;
+    QTest::newRow("single-folder, autosubfolder, existing folder in destination")
+        << QFINDTESTDATA("../kerfuffle/data/one_toplevel_folder.zip") << true << 11 << true << 2;
 
-    QTest::newRow("non single-folder, no autosubfolder") << QFINDTESTDATA("../kerfuffle/data/simplearchive.tar.gz") << false << 4;
+    QTest::newRow("single-folder, autosubfolder, empty destination") << QFINDTESTDATA("../kerfuffle/data/one_toplevel_folder.zip") << true << 9 << false << 1;
 
-    QTest::newRow("non single-folder, autosubfolder") << QFINDTESTDATA("../kerfuffle/data/simplearchive.tar.gz") << true << 5;
+    QTest::newRow("non single-folder, no autosubfolder, empty destination")
+        << QFINDTESTDATA("../kerfuffle/data/simplearchive.tar.gz") << false << 4 << false << 3;
 
-    QTest::newRow("single-file, no autosubfolder") << QFINDTESTDATA("data/test.txt.gz") << false << 1;
+    QTest::newRow("non single-folder, autosubfolder, empty destination") << QFINDTESTDATA("../kerfuffle/data/simplearchive.tar.gz") << true << 5 << false << 1;
 
-    QTest::newRow("single-file, autosubfolder") << QFINDTESTDATA("data/test.txt.gz") << true << 1;
+    QTest::newRow("single-file, no autosubfolder, empty destination") << QFINDTESTDATA("data/test.txt.gz") << false << 1 << false << 1;
+
+    QTest::newRow("single-file, autosubfolder, empty destination") << QFINDTESTDATA("data/test.txt.gz") << true << 1 << false << 1;
 }
 
 void BatchExtractTest::testBatchExtraction()
@@ -70,6 +77,13 @@ void BatchExtractTest::testBatchExtraction()
 
     batchJob->setDestinationFolder(destDir.path());
 
+    QFETCH(bool, createExistingDir);
+    if (createExistingDir) {
+        if (!QDir(destDir.path()).mkdir(QStringLiteral("A"))) {
+            QSKIP("Could not create a temporary directory for existing directory use-case. Skipping test.", SkipSingle);
+        }
+    }
+
     QEventLoop eventLoop(this);
     connect(batchJob, &KJob::result, &eventLoop, &QEventLoop::quit);
     batchJob->start();
@@ -78,13 +92,19 @@ void BatchExtractTest::testBatchExtraction()
     QFETCH(int, expectedExtractedEntriesCount);
     int extractedEntriesCount = 0;
 
-    QDirIterator dirIt(destDir.path(), QDir::AllEntries | QDir::Hidden | QDir::NoDotAndDotDot, QDirIterator::Subdirectories);
-    while (dirIt.hasNext()) {
+    for (const auto _ : QDirListing(destDir.path(), QDirListing::IteratorFlag::Recursive | QDirListing::IteratorFlag::IncludeHidden)) {
         extractedEntriesCount++;
-        dirIt.next();
+    }
+
+    QFETCH(int, expectedTopLevelEntriesCount);
+    int extractedTopLevelEntriesCount = 0;
+
+    for (const auto _ : QDirListing(destDir.path(), QDirListing::IteratorFlag::IncludeHidden)) {
+        extractedTopLevelEntriesCount++;
     }
 
     QCOMPARE(extractedEntriesCount, expectedExtractedEntriesCount);
+    QCOMPARE(extractedTopLevelEntriesCount, expectedTopLevelEntriesCount);
     QCOMPARE(QDir::currentPath(), m_expectedWorkingDir);
 }
 
